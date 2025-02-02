63c01d8e1e3f242585299d38659c21bc981e3a15
matias <mat20can@gmail.com>
2025-02-02 16:44:10 -0300

    version 10.0

--- Main.py
+++ Main.py
@@ -4,5 +4,5 @@ from Space_Pong import *
 if __name__=="__main__":
     best_model = genetic_algorithm((game:=Space_pong_game()), input_size=len(game.get_state()), output_size=2)
     game.model = best_model
-    if game.mode_game[0]:save_model(best_model, torch.optim.Adam(game.model.parameters(), lr=0.001),game.model_path)
+    if game.mode_game["Training AI"]:save_model(best_model, torch.optim.Adam(game.model.parameters(), lr=0.001),game.model_path)
 pygame.quit(),sys.exit()
