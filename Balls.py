7924df16f724b97c2008e701d890acd958dd285a
matias <mat20can@gmail.com>
2025-02-13 17:11:54 -0300

    version 10.0

--- Balls.py
+++ Balls.py
@@ -2,6 +2,8 @@ from pygame import *
 class Ball:
     def __init__(self, x, y, width, height):
         self.rect = Rect(x, y, width, height)
+    def move_ball(self,):
+        pass
     def reset(self, x, y):
         self.rect.x, self.rect.y = x, y
     def check_collision(self, other_rect):
