2479dfe57fc0e2168f7cb9df76e6222c66a49f7d
matias <mat20can@gmail.com>
2025-02-15 12:07:32 -0300

    version 10.0

--- Balls.py
+++ Balls.py
@@ -1,9 +1,9 @@
 from pygame import *
 class Ball:
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height,speedx,speedy):
         self.rect = Rect(x, y, width, height)
-        self.move_x = 4
-        self.move_y = 4
+        self.move_x = speedx
+        self.move_y = speedy
     def move_ball(self,WIDTH,HEIGHT):
         if self.rect.x>=WIDTH-25 or self.rect.x<=0:self.move_x*=-1
         if self.rect.y>=HEIGHT-25 or self.rect.y<=0:self.move_y*=-1
--- Space_Pong.py
+++ Space_Pong.py
@@ -32,7 +32,7 @@ class Space_pong_game(interface):
     def objects(self):
         self.object1=Rect(25,150,11,90)
         self.object2=Rect(665,150,11,90)
-        self.balls=[ Ball(self.WIDTH//2-28,self.HEIGHT//2-29,36,36) for _ in range(1 if self.mode_game["Training AI"] else self.config_game["number_balls"])]
+        self.balls=[ Ball(self.WIDTH//2-28,self.HEIGHT//2-29,36,36,3+i,3+i) for i in range(1 if self.mode_game["Training AI"] else self.config_game["number_balls"])]
     def get_state(self):
         return np.array([self.object1.x, self.object1.y, self.object2.x, self.object2.y,self.balls[0].rect.x,self.balls[0].rect.y])
     def handle_keys(self):
