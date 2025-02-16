2f1ccb781480c147ba0342351fa8cd517a2fb833
matias <mat20can@gmail.com>
2025-02-16 16:14:32 -0300

    version 10.0

--- Players.py
+++ Players.py
@@ -1,8 +1,8 @@
 from pygame import *
 class Player:
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height,active):
         self.rect = Rect(x, y, width, height)
-        self.active=[]
+        self.active=active
         self.score=0
         self.reward=0
     def reset(self,x, y, width, height):
--- Space_Pong.py
+++ Space_Pong.py
@@ -27,11 +27,9 @@ class Space_pong_game(interface):
         self.utils_keys:dict[str,bool]={"UP_W":False,"DOWN_S":False,"UP_ARROW":False,"DOWN_ARROW":False}
         self.key=None
     def objects(self):
-        self.balls=[ Ball(self.WIDTH//2-28,self.HEIGHT//2-29,36,36,4+i,4+i,i) for i in range(1 if self.mode_game["Training AI"] else self.config_game["number_balls"])]
-        self.player_one=Player(25,150,11,90)
-        self.player_one.active.extend([True] * len(self.balls))
-        self.player_two=Player(665,150,11,90)
-        self.player_two.active.extend([True] * len(self.balls))
+        self.balls=[ Ball(self.WIDTH//2-28,self.HEIGHT//2-29,36,36,4+i,4+i) for i in range(1 if self.mode_game["Training AI"] else self.config_game["number_balls"])]
+        self.player_one=Player(25,150,11,90,[True] * len(self.balls))
+        self.player_two=Player(665,150,11,90,[True] * len(self.balls))
     def get_state(self):
         return np.array([self.player_one.rect.x, self.player_one.rect.y, self.player_two.rect.x, self.player_two.rect.y,self.balls[0].rect.x,self.balls[0].rect.y])
     def handle_keys(self):
