cafe5eee3816407f8979dc6c0b886640659815f9
matias <mat20can@gmail.com>
2025-03-08 18:31:46 -0300

    version 10.0

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -2,7 +2,3 @@ import torch
 class AIController():
     def __init__(self):
         pass
-    def action_ai(self,model):
-        state=self.get_state()
-        action = model(torch.tensor(state, dtype=torch.float32)).detach().numpy()
-        self.IA_actions(action)
