04812ea9d2848a05014b085c8ab9eaa786fa78a0
matias <mat20can@gmail.com>
2025-03-12 15:49:56 -0300

    version 10.0

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -6,4 +6,7 @@ class AIHandler:
         self.model
     def get_state(self):
         return np.array([self.player_one.rect.x, self.player_one.rect.y, self.player_two.rect.x, self.player_two.rect.y,self.balls[0].rect.x,self.balls[0].rect.y])
-    
+    def action_ai(self,model):
+        state=self.get_state()
+        action = model(torch.tensor(state, dtype=torch.float32)).detach().numpy()
+        self.IA_actions(action)
