0c079f88173f7b904e5e948f5a0ff360b06d1516
matias <mat20can@gmail.com>
2025-03-12 15:45:07 -0300

    version 10.0

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -4,3 +4,5 @@ class AIHandler:
     def __init__(self, game,model):
         self.game = game
         self.model = model
+    def get_state(self):
+        return np.array([self.player_one.rect.x, self.player_one.rect.y, self.player_two.rect.x, self.player_two.rect.y,self.balls[0].rect.x,self.balls[0].rect.y])
