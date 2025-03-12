847b51d6a11ffb86471a2eb9d6b828c953303231
matias <mat20can@gmail.com>
2025-03-12 15:54:49 -0300

    version 10.0

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -5,11 +5,12 @@ class AIHandler:
         self.game = game
         self.model
     def get_state(self):
-        return np.array([self.player_one.rect.x, self.player_one.rect.y, self.player_two.rect.x, self.player_two.rect.y,self.balls[0].rect.x,self.balls[0].rect.y])
+        return np.array([self.game.player_one.rect.x, self.game.player_one.rect.y, self.game.player_two.rect.x, 
+                        self.game.player_two.rect.y,self.game.balls[0].rect.x,self.game.balls[0].rect.y])
     def action_ai(self,model):
         state=self.get_state()
         action = model(torch.tensor(state, dtype=torch.float32)).detach().numpy()
         self.AI_actions(action)
     def AI_actions(self,action):
-        if action[0]>0 and self.player_two.rect.top > 0:self.player_two.rect.y -= 5
-        if action[0]<0 and self.player_two.rect.bottom < self.HEIGHT:self.player_two.rect.y += 5
+        if action[0]>0 and self.game.player_two.rect.top > 0:self.game.player_two.rect.y -= 5
+        if action[0]<0 and self.game.player_two.rect.bottom < self.game.HEIGHT:self.game.player_two.rect.y += 5
