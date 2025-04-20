39f9e362cb261bc8a562427bb6689d82357cd55b
matias <mat20can@gmail.com>
2025-04-20 16:38:07 -0300

    version 10.0

--- src/AI/Q-learning.py
+++ src/AI/Q-learning.py
@@ -7,3 +7,13 @@ from collections import deque
 from Game.Snake_Game import Snake_Game
 from AI.AI_Controller import AIHandler
 from AI.Neural_Network import SimpleNN
+class ReplayMemory:
+    def __init__(self, capacity: int):
+        self.memory = deque(maxlen=capacity)
+    def push(self, transition: tuple):
+        self.memory.append(transition)
+    def sample(self, batch_size: int):
+        return random.sample(self.memory, batch_size)
+    def __len__(self) -> int:
+        return len(self.memory)
+
