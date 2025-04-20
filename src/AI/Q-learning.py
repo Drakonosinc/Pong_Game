1956bfa525043b97dfb68b690078adf43d159c2b
matias <mat20can@gmail.com>
2025-04-20 16:45:21 -0300

    version 10.0

--- src/AI/Q-learning.py
+++ src/AI/Q-learning.py
@@ -4,8 +4,8 @@ import torch.optim as optim
 import random
 import numpy as np
 from collections import deque
-from Game.Snake_Game import Snake_Game
-from AI.AI_Controller import AIHandler
+from Game.Space_Pong import Snake_Game
+from AI.Ai_Controller import AIHandler
 from AI.Neural_Network import SimpleNN
 class ReplayMemory:
     def __init__(self, capacity: int):
