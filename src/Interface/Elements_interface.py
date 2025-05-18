ae1337bf960377feb77a850d4aa544fd15256576
matias <mat20can@gmail.com>
2025-05-18 16:13:37 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -128,7 +128,7 @@ class Input_text(ElementBehavior):
     def draw(self):
         pygame.draw.rect(self.screen,self.color_back,self.rect)
         if self.detect_mouse:self.mouse_collision(self.rect,pygame.mouse.get_pos())
-        if self.pressed:self.pressed_button(pygame.mouse.get_pressed(),pygame.mouse.get_pos())
+        if self.pressed:self.pressed_button(self.rect,pygame.mouse.get_pressed(),pygame.mouse.get_pos())
         input_player=pygame.draw.rect(self.screen,self.border_color,self.rect,self.border)
         self.screen.blit(self.font.render(self.text, True, self.color), (input_player.x+5, input_player.y-2))
     def draw_hover_effect(self):return pygame.draw.rect(self.screen,self.hover_color,self.rect)
