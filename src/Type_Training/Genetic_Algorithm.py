0f832944f05311c289f9a075a40ed8c7941e0461
matias <mat20can@gmail.com>
2025-05-25 22:28:53 -0300

    version 10.0

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -1,5 +1,5 @@
 import random,copy
-from .Neural_Network import *
+from Type_Model import *
 # Función de fitness
 def fitness_function(model, game):
     game.model = model  # Asigna el modelo al juego antes de ejecutarlo
