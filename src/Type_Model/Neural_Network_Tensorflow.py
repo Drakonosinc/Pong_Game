d07d8aed02474433d925536995db930a15f85d41
matias <mat20can@gmail.com>
2025-05-26 13:38:32 -0300

    version 10.0

--- src/Type_Model/Neural_Network_Tensorflow.py
+++ src/Type_Model/Neural_Network_Tensorflow.py
@@ -7,3 +7,13 @@ class SimpleNN(Model):
         self.fc1 = layers.Dense(128, activation='relu', input_shape=(input_size,))
         self.fc2 = layers.Dense(output_size)
         self.activations = None
+    def call(self, x):
+        x = self.fc1(x)
+        self.activations = self.fc1.output
+        if hasattr(x, 'numpy'):
+            activations_np = self.activations.numpy().reshape(1, -1)
+            min_val = np.min(activations_np)
+            max_val = np.max(activations_np)
+            self.activations = (activations_np - min_val) / (max_val - min_val)
+        x = self.fc2(x)
+        return x
