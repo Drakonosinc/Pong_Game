cc8877b880b5db6966d74eaec7d7c330841665e7
matias <mat20can@gmail.com>
2025-06-03 16:03:13 -0300

    version 10.0

--- src/Interface/Menus/Base_Menu.py
+++ src/Interface/Menus/Base_Menu.py
@@ -7,3 +7,13 @@ class BaseMenu:
         background=pygame.Surface((self.WIDTH,self.HEIGHT),pygame.SRCALPHA)
         background.fill((0,0,0,number))
         self.screen.blit(background,(0,0))
+    def fade_transition(self,fade_in,color=(0,0,0),limit=255):
+        overlay = pygame.Surface((self.WIDTH, self.HEIGHT))
+        overlay.fill(color)
+        alpha=0 if not fade_in else 255
+        while (not fade_in and alpha <= limit) or (fade_in and alpha >= limit):
+            overlay.set_alpha(alpha)
+            self.screen.blit(overlay, (0, 0))
+            pygame.display.flip()
+            self.clock.tick(20)
+            alpha += -15 if fade_in else 15
