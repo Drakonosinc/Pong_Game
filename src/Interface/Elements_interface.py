ebf4ca3f349b37ee0c952cb566ced2f7440b8fed
matias <mat20can@gmail.com>
2025-06-03 15:35:55 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -45,8 +45,7 @@ class ElementBehavior:
                 if self.sound_hover:self.sound_hover.play(loops=0)
                 self.states["detect_hover"]=False
         else:self.states["detect_hover"]=True
-    def rects_collision(self,dict_rects:dict):
-        pass
+    def rects_collision(self,dict_rects:dict):pass
     def pressed_button(self,rect,pressed_mouse,mouse_pos,draw=None):
         current_time = pygame.time.get_ticks()
         if pressed_mouse[0] and rect.collidepoint(mouse_pos) and self.states["presses_touch"]:
