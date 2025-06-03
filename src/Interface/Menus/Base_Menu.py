ee6f373905d895b7c32d7901569436b09a8952b4
matias <mat20can@gmail.com>
2025-06-03 16:02:38 -0300

    version 10.0

--- src/Interface/Menus/Base_Menu.py
+++ src/Interface/Menus/Base_Menu.py
@@ -3,4 +3,7 @@ class BaseMenu:
         self.interface = interface
     def execute_buttons(self,*args):
         for button in args:button.draw()
-    
+    def filt(self,number):
+        background=pygame.Surface((self.WIDTH,self.HEIGHT),pygame.SRCALPHA)
+        background.fill((0,0,0,number))
+        self.screen.blit(background,(0,0))
