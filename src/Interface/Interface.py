525f7e2bec9ee4be96acb369e0460a160259ce80
matias <mat20can@gmail.com>
2025-06-03 16:06:36 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -115,10 +115,6 @@ class interface(load_elements):
         self.exit_button=self.button_factory_f2_5.create_TextButton({"text": "Exit","color": self.GRAY,"position": (self.WIDTH/2-40,self.HEIGHT/2-15),"color2": self.SKYBLUE,"sound_touch":None,"command1":self.event_quit})
         self.reset_pause_button=self.button_factory_f2_5.create_TextButton({"text": "Reset","color": self.GRAY,"position": (self.WIDTH/2-55,self.HEIGHT/2-85),"color2": self.SKYBLUE,"command1":lambda:self.change_mains({"main":-1}),"command2":self.reset})
         self.go_main_button=self.button_factory_f2_5.create_TextButton({"text": "Menu","color": self.GRAY,"position": (self.WIDTH/2-45,self.HEIGHT/2-50),"color2": self.SKYBLUE,"command1":lambda:self.change_mains({"main":0,"run":True}),"command2":self.reset})
-    def filt(self,number):
-        background=pygame.Surface((self.WIDTH,self.HEIGHT),pygame.SRCALPHA)
-        background.fill((0,0,0,number))
-        self.screen.blit(background,(0,0))
     def options_menu(self):
         self.screen.fill(self.BLACK)
         self.sound_button.change_item({"color":self.sound_type["color"],"text":self.sound_type["sound"]})
@@ -187,25 +183,3 @@ class interface(load_elements):
             self.change_keys(self.key,self.key_name)
     def check_item(self,dic,is_true,is_false,item,**kwargs):
         for key,button in kwargs.items():setattr(button,item,(is_true if dic[key] else is_false))
-    def fade_transition(self,fade_in,color=(0,0,0),limit=255):
-        overlay = pygame.Surface((self.WIDTH, self.HEIGHT))
-        overlay.fill(color)
-        alpha=0 if not fade_in else 255
-        while (not fade_in and alpha <= limit) or (fade_in and alpha >= limit):
-            overlay.set_alpha(alpha)
-            self.screen.blit(overlay, (0, 0))
-            pygame.display.flip()
-            self.clock.tick(20)
-            alpha += -15 if fade_in else 15
-    def change_mains(self,config):
-        if fade_in:=config.get("fade_in",True):self.fade_transition(False,config.get("color",(0,0,0)),255)
-        if fade_out:=config.get("fade_out",False):self.fade_transition(True,config.get("color2",(0,0,0)),0)
-        self.main=config.get("main",None)
-        if config.get("command",None):config["command"]()
-        if config.get("run",False):setattr(self,"running",False),setattr(self, "game_over", True)
-        if config.get("recursive",False):self.change_mains({"main":self.main,"fade_in":fade_in,"fade_out":fade_out})
-    def increase_decrease_variable(self,dic=None,variable="",length=None,number=1,save=True):
-        if dic!=None and length!=None:dic[variable]=max(1, dic[variable] + number)
-        elif dic!=None:dic[variable]+=number
-        else:setattr(self,variable,getattr(self,variable)+number)
-        if save:self.config.save_config()
