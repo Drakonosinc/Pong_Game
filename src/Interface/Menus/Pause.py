64266a93579af44755d8253b9409f4be9c5f5dd0
matias <mat20can@gmail.com>
2025-06-04 16:31:36 -0300

    version 10.0

--- src/Interface/Menus/Pause.py
+++ src/Interface/Menus/Pause.py
@@ -1,2 +1,7 @@
-class Pause:
-    def __init__(self):pass
+from .Base_Menu import BaseMenu
+class Pause(BaseMenu):
+    def __init__(self, interface):
+        super().__init__(interface)
+        self.buttons = {}
+    def setup_buttons(self):pass
+    def render(self):pass
