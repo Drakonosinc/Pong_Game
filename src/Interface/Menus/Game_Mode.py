e0f8f082e4e6f447b628bd9c951c573abcfca136
matias <mat20can@gmail.com>
2025-06-05 11:43:46 -0300

    version 10.0

--- src/Interface/Menus/Game_Mode.py
+++ src/Interface/Menus/Game_Mode.py
@@ -9,4 +9,6 @@ class GameMode(BaseMenu):
         factory = self.interface.button_factory_f5
         self.buttons['back'] = factory.create_PolygonButton({"position": ((50, 350), (50, 380), (25, 365)),"position2":((50, 340), (50, 390), (10, 365)),"command1":lambda:self.change_mains({"main":0})})
         self.buttons['continue'] = factory.create_PolygonButton({"position": ((650, 350), (650, 380), (675, 365)),"position2":((650, 340), (650, 390), (690, 365)),"command1":lambda:self.change_mains({"main":-1,"run":True,"command":self.interface.objects})})
+        self.interface.back_button = self.buttons['back']
+        self.interface.continue_button = self.buttons['continue']
     def render(self):pass
