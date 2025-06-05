e887709cf9424f8af49f3efb02201b6bf083dfaf
matias <mat20can@gmail.com>
2025-06-05 11:42:35 -0300

    version 10.0

--- src/Interface/Menus/Game_Mode.py
+++ src/Interface/Menus/Game_Mode.py
@@ -7,4 +7,6 @@ class GameMode(BaseMenu):
         self._setup_navigation_buttons()
     def _setup_navigation_buttons(self):
         factory = self.interface.button_factory_f5
+        self.buttons['back'] = factory.create_PolygonButton({"position": ((50, 350), (50, 380), (25, 365)),"position2":((50, 340), (50, 390), (10, 365)),"command1":lambda:self.change_mains({"main":0})})
+        
     def render(self):pass
