6bcded01ca1db2ee122fbd8b68e4febd2af273a8
matias <mat20can@gmail.com>
2025-06-06 13:18:53 -0300

    version 10.0

--- src/Interface/Menus/Game_Mode.py
+++ src/Interface/Menus/Game_Mode.py
@@ -64,6 +64,7 @@ class GameMode(BaseMenu):
     def _setup_training_ai_texts(self):
         factory = self.interface.button_factory_f5
         self.training_ai_elements['text_C'] = factory.create_Text({"text": f"Config Training\n{'AI':^26}","position": (self.WIDTH/2+120, self.HEIGHT/2-136),"detect_mouse": False})
+        self.training_ai_elements['text_G'] = factory.create_Text({"text": f"Generation Size\n{self.config.config_AI['generation_value']:^26}","position": (self.WIDTH/2+120, self.HEIGHT/2-81),"detect_mouse": False})
         
     def _setup_scroll_bar(self):pass
     def render(self):pass
