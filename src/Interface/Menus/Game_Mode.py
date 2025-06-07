e776fe095dd00f40072ce249d053937d9d9ee957
matias <mat20can@gmail.com>
2025-06-07 15:47:37 -0300

    version 10.0

--- src/Interface/Menus/Game_Mode.py
+++ src/Interface/Menus/Game_Mode.py
@@ -98,4 +98,5 @@ class GameMode(BaseMenu):
         self.screen.blit(self.interface.font5.render("Enter Player Name One", True, "white"), (7, 10))
         self.screen.blit(self.interface.font5.render("Enter Player Name Two", True, "white"), (416, 10))
         self.screen.blit(font_modegame.render("Max Score", True, "white"), (self.WIDTH/2-68, self.HEIGHT/2-50))
+        self.screen.blit(font_modegame.render(f"{self.config.config_game['max_score']}", True, "white"), (self.WIDTH/2-8, self.HEIGHT/2-20))
     def _render_training_ai(self):pass
