7d3c2794da5457411df1a09f035bbc07a31494be
matias <mat20can@gmail.com>
2025-06-08 15:20:56 -0300

    version 10.0

--- src/Interface/Menus/Pause.py
+++ src/Interface/Menus/Pause.py
@@ -8,5 +8,7 @@ class Pause(BaseMenu):
         self.buttons['exit'] = factory.create_TextButton({"text": "Exit","color": self.interface.GRAY,"position": (self.WIDTH/2-40, self.HEIGHT/2-15),"color2": self.interface.SKYBLUE,"sound_touch": None,"command1": self.interface.event_quit})
         self.buttons['reset'] = factory.create_TextButton({"text": "Reset","color": self.interface.GRAY,"position": (self.WIDTH/2-55, self.HEIGHT/2-85),"color2": self.interface.SKYBLUE,"command1": lambda: self.change_mains({"main": -1}),"command2": self.interface.reset})
         self.buttons['menu'] = factory.create_TextButton({"text": "Menu","color": self.interface.GRAY,"position": (self.WIDTH/2-45, self.HEIGHT/2-50),"color2": self.interface.SKYBLUE,"command1": lambda: self.change_mains({"main": 0, "run": True}),"command2": self.interface.reset})
-        
+        self.interface.exit_button = self.buttons['exit']
+        self.interface.reset_pause_button = self.buttons['reset']
+        self.interface.go_main_button = self.buttons['menu']
     def render(self):pass
