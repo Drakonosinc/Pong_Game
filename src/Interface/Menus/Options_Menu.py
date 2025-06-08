76922c1b75d77b0bfca2be3d0a2d9cfa2aa0473a
matias <mat20can@gmail.com>
2025-06-08 15:31:24 -0300

    version 10.0

--- src/Interface/Menus/Options_Menu.py
+++ src/Interface/Menus/Options_Menu.py
@@ -4,3 +4,6 @@ class OptionsMenu(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
         self.buttons = {}
+    def setup_buttons(self):
+        factory_f5 = self.interface.button_factory_f5
+        factory_f2_5 = self.interface.button_factory_f2_5
