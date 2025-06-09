461f4dff918052b8d929d90de2709027957446db
matias <mat20can@gmail.com>
2025-06-09 13:33:14 -0300

    version 10.0

--- src/Interface/Menus/Visuals_Menu.py
+++ src/Interface/Menus/Visuals_Menu.py
@@ -8,4 +8,5 @@ class VisualsMenu(BaseMenu):
         factory_f5 = self.interface.button_factory_f5
         factory_f2_5 = self.interface.button_factory_f2_5
         self.buttons['back'] = factory_f5.create_PolygonButton({"position": ((50, 350), (50, 380), (25, 365)),"position2": ((50, 340), (50, 390), (10, 365)),"command1": lambda: self.change_mains({"main": 4})})
+        self.buttons['decrease_width'] = factory_f2_5.create_TextButton({"text": "<","position": (self.WIDTH/2-200, self.HEIGHT/2-200),"command1": lambda: self._change_items("WIDTH", number=-10)})
         
