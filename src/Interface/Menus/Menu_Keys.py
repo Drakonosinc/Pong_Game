8c8052690defade7f0c775aeb4c2e01b027c06f9
matias <mat20can@gmail.com>
2025-06-10 15:23:39 -0300

    version 10.0

--- src/Interface/Menus/Menu_Keys.py
+++ src/Interface/Menus/Menu_Keys.py
@@ -1,2 +1,10 @@
-class MenuKeys:
-    def __init__(self):pass
+import pygame
+from pygame.locals import KEYDOWN
+from .Base_Menu import BaseMenu
+class KeysMenu(BaseMenu):
+    def __init__(self, interface):
+        super().__init__(interface)
+        self.buttons = {}
+        self.key = None
+        self.key_name = None
+        self.button_key = None
