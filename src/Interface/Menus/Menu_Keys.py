f4cd597e86f6ce9eb646bb7fc41f41eb7b64fca5
matias <mat20can@gmail.com>
2025-06-10 15:34:59 -0300

    version 10.0

--- src/Interface/Menus/Menu_Keys.py
+++ src/Interface/Menus/Menu_Keys.py
@@ -13,6 +13,7 @@ class KeysMenu(BaseMenu):
         self.buttons['back'] = factory_f5.create_PolygonButton({"position": ((50, 350), (50, 380), (25, 365)),"position2": ((50, 340), (50, 390), (10, 365)),"command1": lambda: self.change_mains({"main": 4})})
         self.buttons['up_w'] = factory_f5.create_TextButton({"font": self.interface.font4_5,"text": self.config.config_keys["Name_key1"],"position": (self.WIDTH/2-240, self.HEIGHT/2-170),"command1": lambda: self._change_keys("UP_W", "Name_key1", self.buttons['up_w'])})
         self.buttons['down_s'] = factory_f5.create_TextButton({"font": self.interface.font4_5,"text": self.config.config_keys["Name_key2"],"position": (self.WIDTH/2-217, self.HEIGHT/2-20),"command1": lambda: self._change_keys("DOWN_S", "Name_key2", self.buttons['down_s'])})
+        self.buttons['up_arrow'] = factory_f5.create_TextButton({"font": self.interface.font4_5,"text": self.config.config_keys["Name_key3"],"position": (self.WIDTH/2+200, self.HEIGHT/2-170),"command1": lambda: self._change_keys("UP_ARROW", "Name_key3", self.buttons['up_arrow'])})
         
     
     
