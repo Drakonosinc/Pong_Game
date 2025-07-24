db36f84cfb9ad9c2f5409e5fa628b503993dcccf
matias <mat20can@gmail.com>
2025-07-24 15:18:57 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -212,7 +212,6 @@ class ComboBox(TextButton):
         self.is_dropdown_open = False
         self.selected_index = None
         self.options = []
-        self.option_buttons = []
         rect_base = pygame.Rect(*self.position,*self.font.size(self.text))
         rect_dropdown = pygame.Rect(self.position[0]+self.font.size(self.text)[0], self.position[1],*self.font.size(self.type_dropdown))
         self.rect = {"base": rect_base, "dropdown": rect_dropdown}
