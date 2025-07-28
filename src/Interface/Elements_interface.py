e428e74c475fb591a9f14e60cc923185dcdfdd1b
matias <mat20can@gmail.com>
2025-07-28 18:02:47 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -238,7 +238,10 @@ class ComboBox(TextButton):
         if self.detect_mouse:self.mouse_collision(self.rect["button"],pygame.mouse.get_pos(),self.draw_hover_effect)
         if self.pressed:self.pressed_button(self.rect["button"],pygame.mouse.get_pressed(),pygame.mouse.get_pos())
     def draw_hover_effect(self):return self.screen.blit(self.font.render(f"{self.text}{self.type_dropdown}", True,self.hover_color), (self.position))
-    
+    def draw_rect_dropdown(self):
+        self.dropdown_rect = pygame.Rect(self.position[0], self.position[1] + self.font.get_height(), *self.dropdown)
+        pygame.draw.rect(self.screen, self.hover_color, self.dropdown_rect)
+        # for button in self.option_buttons:button.draw()
     def charge_elements(self, options: list[str]):
         self.options = options
         self.option_buttons = []
