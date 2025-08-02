63dfdd92161904f32bb71cfbbad82d1f11238642
matias <mat20can@gmail.com>
2025-08-02 16:08:35 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -255,7 +255,9 @@ class ComboBox(TextButton):
         self.button_dropdown.change_item({"color": self.hover_dropdown})
         self.dropdown_rect = self.get_rect_dropdown()
         pygame.draw.rect(self.screen, self.hover_dropdown, self.dropdown_rect)
-        for button in self.option_buttons:button.draw()
+        for i,button in enumerate(self.option_buttons):
+            button.change_item({"position": (self.position[0], self.position[1] + self.font.get_height() + i * (self.font.get_height() + 5))})
+            button.draw()
     def charge_elements(self, options: list[str]):
         self.options = options
         for i, option in enumerate(options):
