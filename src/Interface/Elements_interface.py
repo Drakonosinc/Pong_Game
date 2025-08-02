a8f4a5c49c2be469932f2239a9006edccc967f5e
matias <mat20can@gmail.com>
2025-08-02 16:53:15 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -263,13 +263,12 @@ class ComboBox(TextButton):
         for i, option in enumerate(options):
             x = self.position[0]
             y = self.position[1] + self.font.get_height() + i * (self.font.get_height() + 5)
-            position = (x, y)
             button = TextButton({
                 "screen": self.screen,
                 "font": self.font,
                 "color": self.color,
                 "hover_color": self.hover_color,
-                "position": position,
+                "position": (x, y),
                 "text": option,
                 "command1": lambda idx=i: self.select_option(idx) if self.replace_text else None})
             # if y>self.dropdown[1]:
