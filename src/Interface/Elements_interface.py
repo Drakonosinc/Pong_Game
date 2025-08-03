415eafe84c029eef42833b9581445841414cff88
matias <mat20can@gmail.com>
2025-08-03 18:02:56 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -165,7 +165,7 @@ class ScrollBar(ElementBehavior):
         elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:self.dragging = False
         elif event.type == pygame.MOUSEMOTION and self.dragging:
             new_y = event.pos[1] - self.drag_offset
-            new_y = max(self.rect.top, min(new_y, self.rect.bottom - self.thumb_height))
+            new_y = max(self.rect["rect"].top, min(new_y, self.rect["rect"].bottom - self.thumb_height))
             self.thumb_rect.y = new_y
             self.scroll_elements()
     def scroll_elements(self):
