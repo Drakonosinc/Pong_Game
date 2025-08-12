cc19a6578e46dac0c4ff3c7b2c11fdd0e4408381
matias <mat20can@gmail.com>
2025-08-12 16:55:06 -0300

    version 10.0

--- src/Interface/Menus/Game_Mode.py
+++ src/Interface/Menus/Game_Mode.py
@@ -62,13 +62,13 @@ class GameMode(BaseMenu):
         self.config_buttons['decrease_try_for_ai'] = factory.create_TextButton({"text": "<","position": (self.WIDTH-178, self.HEIGHT/2+55),"command1": lambda: self.increase_decrease_variable(self.config.config_AI, 'try_for_ai', True, -1),"command2": self._update_training_ai_texts})
         self.config_buttons['save_model'] = factory.create_TextButton({"text": "OFF","color": self.interface.SKYBLUE,"position": (self.WIDTH-85, self.HEIGHT/2+84),"command1": lambda: self.on_off(self.config.config_AI, "model_save"),"command2": self.config.save_config})
         self.config_buttons['box_type_model'] = factory.create_ComboBox({"text": "Model","position": (self.WIDTH/2+120, self.HEIGHT/2+139)})
-        self.config_buttons['box_type_model'].charge_elements({"Pytorch":lambda:self._update_model_ai(), "Tensorflow":lambda:self._update_model_ai()})
+        self.config_buttons['box_type_model'].charge_elements({"Pytorch":lambda:self._update_model_ai("Pytorch"), "Tensorflow":lambda:self._update_model_ai("Tensorflow")})
         self.interface.box_type_model = self.config_buttons['box_type_model']
         for key, button in self.config_buttons.items():setattr(self.interface, key, button)
-    def _update_model_ai(self):
+    def _update_model_ai(self,button):
         model_ai = {"Pytorch": self.config_buttons['box_type_model'].return_buttons("Pytorch"),"Tensorflow": self.config_buttons['box_type_model'].return_buttons("Tensorflow")}
-        self.check_item(self.config.config_AI["type_model"],self.interface.SKYBLUE,self.interface.WHITE,"color",**model_ai)
-        for b in self.config.config_AI["type_model"].values():self.config.config_AI["type_model"][b] = False if b != button else True
+        self.check_item(self.config.config_AI["type_model"],self.interface.RED,self.interface.WHITE,"color",**model_ai)
+        for b in self.config.config_AI["type_model"].keys():self.config.config_AI["type_model"][b] = False if b != button else True
     def _setup_training_ai_texts(self):
         factory = self.interface.button_factory_f5
         self.training_ai_elements['text_C'] = factory.create_Text({"text": f"Config Training\n{'AI':^26}","position": (self.WIDTH/2+120, self.HEIGHT/2-136),"detect_mouse": False})
