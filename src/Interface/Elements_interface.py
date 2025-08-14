47fd02cb253a8b34b2a765ba5d6d20df96caf390
matias <mat20can@gmail.com>
2025-08-14 15:55:11 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -295,6 +295,7 @@ class ComboBox(TextButton):
     def charge_buttons(self,buttons: list, scroll: bool = True):
         for i, button in enumerate(buttons):
             button.position = (self.position[0], self.position[1] + self.font.get_height() + i * (self.font.get_height() + 5))
+            self.option_buttons[button.text] = button
             self.rect[f"option_{i}"] = button
             if len(self.options[i]) >= len(button.text):self.dropdown[0] = self.font.size(button.text)[0] + 5
         self.dropdown[1] = len(self.option_buttons) * (self.font.get_height() + 5)
