3a07ccf9607806430374fa4f25cfd8cf820cf4ef
matias <mat20can@gmail.com>
2025-08-19 19:43:54 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -275,7 +275,8 @@ class ComboBox(TextButton):
         self.dropdown_rect = self.get_rect_dropdown()
         pygame.draw.rect(self.screen, self.hover_dropdown, self.dropdown_rect)
         pygame.draw.rect(self.screen, self.color, self.dropdown_rect, 2)
-        for button in self.option_buttons.values():
+        for i,button in enumerate(self.option_buttons.values()):
+            if len(self.options[i]) >= len(button.text):self.dropdown[0] = self.font.size(button.text)[0] + 5
             button.draw()
             # if button.rect.bottom<=self.dropdown_rect.bottom and button.rect.top>=self.dropdown_rect.top:button.draw()
         if hasattr(self, 'scroll'):
@@ -289,8 +290,6 @@ class ComboBox(TextButton):
                 "command1": lambda idx=i: self.select_option(idx) if self.replace_text else None,
                 "command2": action if callable(action) else None})
             self._repeat_charge(f"elements_{i}",option,button)
-            self.options.append(option)
-            if len(self.options[i]) >= len(option):self.dropdown[0] = self.font.size(option)[0] + 5
         if self.adapt_dropdown:self.dropdown[1] = len(self.option_buttons) * (self.font.get_height() + 5)
         if self.draw_scroll:self._create_scroll()
         if (options and not self.text) and self.replace_text:
@@ -301,12 +300,12 @@ class ComboBox(TextButton):
             if not self.option_buttons:button.position = (self.position[0], self.position[1] + self.font.get_height() + i * (self.font.get_height() + 5))
             else:button.position = (self.position[0], self.option_buttons[list(self.option_buttons.keys())[-1]].rect.bottom + 5)
             self._repeat_charge(f"buttons_{i}",button.text,button)
-            if len(buttons[i].text) >= len(button.text):self.dropdown[0] = self.font.size(button.text)[0] + 5
         if self.adapt_dropdown:self.dropdown[1] = len(self.option_buttons) * (self.font.get_height() + 5)
         if self.draw_scroll:self._create_scroll()
     def _repeat_charge(self,i,option,button):
         self.option_buttons[option] = button
         self.rect[i] = button
+        self.options.append(option)
     def _create_scroll(self):
         self.scroll = self.factory.create_ScrollBar({
             "position": (self.position[0] + self.dropdown[0], self.position[1] + self.font.get_height(), 20, self.dropdown[1]),
