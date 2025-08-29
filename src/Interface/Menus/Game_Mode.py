361207c08c841897dd276f1cd195672cc479fc87
matias <mat20can@gmail.com>
2025-08-29 17:26:06 -0300

    version 10.0

--- src/Interface/Menus/Game_Mode.py
+++ src/Interface/Menus/Game_Mode.py
@@ -61,7 +61,7 @@ class GameMode(BaseMenu):
         self.config_buttons['increase_try_for_ai'] = factory.create_TextButton({"text": ">","position": (self.WIDTH-100, self.HEIGHT/2+55),"command1": lambda: self.increase_decrease_variable(self.config.config_AI, 'try_for_ai'),"command2": self._update_training_ai_texts})
         self.config_buttons['decrease_try_for_ai'] = factory.create_TextButton({"text": "<","position": (self.WIDTH-178, self.HEIGHT/2+55),"command1": lambda: self.increase_decrease_variable(self.config.config_AI, 'try_for_ai', True, -1),"command2": self._update_training_ai_texts})
         self.config_buttons['save_model'] = factory.create_TextButton({"text": "OFF","color": self.interface.SKYBLUE,"position": (self.WIDTH-85, self.HEIGHT/2+84),"command1": lambda: self.on_off(self.config.config_AI, "model_save"),"command2": self.config.save_config})
-        self.config_buttons['box_type_training'] = factory.create_ComboBox({"text": "Training","position": (20, self.HEIGHT/2-136)})
+        self.config_buttons['box_type_training'] = factory.create_ComboBox({"text": "Training","position": (5, self.HEIGHT/2-136)})
         self.config_buttons['box_type_model'] = factory.create_ComboBox({"text": "Model","position": (self.WIDTH/2+120, self.HEIGHT/2+139)})
         self.config_buttons['box_type_model'].charge_elements({"Pytorch":lambda:self._update_model_ai("Pytorch"), "Tensorflow":lambda:self._update_model_ai("Tensorflow")})
         self._update_model_ai("Pytorch")
@@ -126,7 +126,7 @@ class GameMode(BaseMenu):
         if self.interface.mode_game["Training AI"]:self._execute_training_ai_buttons()
         else:self._execute_game_config_buttons()
     def _execute_training_ai_buttons(self):
-        training_buttons = [self.config_buttons['increase_generation'],self.config_buttons['decrease_generation'],self.config_buttons['increase_population'],self.config_buttons['decrease_population'],self.config_buttons['increase_try_for_ai'],self.config_buttons['decrease_try_for_ai'],self.config_buttons['save_model'],self.config_buttons['scroll'],self.config_buttons['box_type_model']]
+        training_buttons = [self.config_buttons['increase_generation'],self.config_buttons['decrease_generation'],self.config_buttons['increase_population'],self.config_buttons['decrease_population'],self.config_buttons['increase_try_for_ai'],self.config_buttons['decrease_try_for_ai'],self.config_buttons['save_model'],self.config_buttons['scroll'],self.config_buttons["box_type_training"],self.config_buttons['box_type_model']]
         for button in training_buttons:button.draw()
     def _execute_game_config_buttons(self):
         config_buttons = [self.config_buttons['increase_balls'],self.config_buttons['decrease_balls']]
