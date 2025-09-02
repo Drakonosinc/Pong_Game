036a65eb831c16826107811a093428d1630f0ac8
matias <mat20can@gmail.com>
2025-09-02 17:13:39 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -282,7 +282,7 @@ class ComboBox(TextButton):
         pygame.draw.rect(self.screen, self.color, self.dropdown_rect, 2)
         if self.adapt_dropdown and self.option_buttons and self.type_dropdown in (" V", " ^"):self.dropdown[1], self.adapt_dropdown = (len(self.option_buttons) * (self.font.get_height() + 5)), False
         elif self.adapt_dropdown and self.option_buttons:
-            self.dropdown[0], self.dropdown[1], self.adapt_dropdown = (len(self.option_buttons) * (self.font.size(list(self.option_buttons.keys())[-1])[0])), (self.font.get_height() + 5), False
+            self.dropdown[0], self.dropdown[1], self.adapt_dropdown = ((len(self.option_buttons) * (self.font.size(list(self.option_buttons.keys())[-1])[0])) - 25), (self.font.get_height() + 5), False
         if self.draw_scroll and self.option_buttons:self._create_scroll()
         for button in self.option_buttons.values():
             if (button.rect.bottom<=self.dropdown_rect.bottom and button.rect.top>=self.dropdown_rect.top) and (self.type_dropdown in (" V", " ^")):button.draw()
