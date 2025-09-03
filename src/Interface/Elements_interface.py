936028e8c44c0d2f819aba87692d0361691015b9
matias <mat20can@gmail.com>
2025-09-03 16:15:14 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -289,7 +289,7 @@ class ComboBox(TextButton):
         else:button.draw()
     def _adapt_size_dropdown(self):
         if self.adapt_dropdown and self.option_buttons and self.type_dropdown in (" V", " ^"):self.dropdown[1], self.adapt_dropdown = (len(self.option_buttons) * (self.font.get_height() + 5)), False
-        elif self.adapt_dropdown and self.option_buttons:self.dropdown[0], self.dropdown[1], self.adapt_dropdown = (self.option_buttons[list(self.option_buttons.keys())[0]].rect.right-self.option_buttons[list(self.option_buttons.keys())[-1]].rect.left), (self.font.get_height() + 5), False
+        elif self.adapt_dropdown and self.option_buttons and self.type_dropdown == " <":self.dropdown[0], self.dropdown[1], self.adapt_dropdown = (self.option_buttons[list(self.option_buttons.keys())[0]].rect.right-self.option_buttons[list(self.option_buttons.keys())[-1]].rect.left) + 10, (self.font.get_height() + 5), False
     def _draw_scroll(self):
         if self.type_dropdown == " V":self.scroll.rect["rect"].height = self.dropdown_rect.height
         elif self.type_dropdown == " ^":
