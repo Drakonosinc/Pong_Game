d915790bfbf27c1424df95c672eb54d6d178b060
matias <mat20can@gmail.com>
2025-09-07 16:21:55 -0300

    version 10.0

--- src/Interface/Menus/Game_Mode.py
+++ src/Interface/Menus/Game_Mode.py
@@ -81,9 +81,9 @@ class GameMode(BaseMenu):
     def _setup_training_ai_texts(self):
         factory = self.interface.button_factory_f5
         self.training_ai_elements['text_C'] = factory.create_Text({"text": f"Config Training\n{'AI':^26}","position": (self.WIDTH/2+120, self.HEIGHT/2-136),"detect_mouse": False})
-        self.training_ai_elements['text_G'] = factory.create_Text({"text": f"Generation Size\n{self.config.config_AI['generation_value']:^26}","position": (self.WIDTH/2+120, self.HEIGHT/2-81),"detect_mouse": False})
-        self.training_ai_elements['text_P'] = factory.create_Text({"text": f"Population Size\n{self.config.config_AI['population_value']:^26}","position": (self.WIDTH/2+120, self.HEIGHT/2-26),"detect_mouse": False})
-        self.training_ai_elements['text_A'] = factory.create_Text({"text": f"Attempts By AI\n{self.config.config_AI['try_for_ai']:^{28 if self.config.config_AI['try_for_ai']<10 else 26}}","position": (self.WIDTH/2+120, self.HEIGHT/2+29),"detect_mouse": False})
+        self.training_ai_elements['text_G'] = factory.create_Text({"text": f"Generation Size\n{self.config.config_AI["genetic"]['generation_value']:^26}","position": (self.WIDTH/2+120, self.HEIGHT/2-81),"detect_mouse": False})
+        self.training_ai_elements['text_P'] = factory.create_Text({"text": f"Population Size\n{self.config.config_AI["genetic"]['population_value']:^26}","position": (self.WIDTH/2+120, self.HEIGHT/2-26),"detect_mouse": False})
+        self.training_ai_elements['text_A'] = factory.create_Text({"text": f"Attempts By AI\n{self.config.config_AI["genetic"]['try_for_ai']:^{28 if self.config.config_AI["genetic"]['try_for_ai']<10 else 26}}","position": (self.WIDTH/2+120, self.HEIGHT/2+29),"detect_mouse": False})
         self.training_ai_elements['text_S'] = factory.create_Text({"text": "Save model","position": (self.WIDTH/2+120, self.HEIGHT/2+84),"detect_mouse": False})
         self.interface.text_in_training_ai = list(self.training_ai_elements.values())
     def _setup_scroll_bar(self):
