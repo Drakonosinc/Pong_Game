b6fd72bd1d9ca79ccf34ca7edf6d64f8574f4483
matias <mat20can@gmail.com>
2025-09-07 16:13:56 -0300

    version 10.0

--- src/Main.py
+++ src/Main.py
@@ -12,7 +12,7 @@ if __name__=="__main__":
                     q_config = game.config.config_AI["q_learning"]
                     best_model = q_learning_algorithm(game, input_size=len(game.ai_handler.get_state()), output_size=2, episodes=q_config["episodes"],lr=q_config["learning_rate"],gamma=q_config["gamma"],epsilon_start=q_config["epsilon_start"],epsilon_end=q_config["epsilon_end"],epsilon_decay=q_config["epsilon_decay"])
                 game.model = best_model
-                if game.config.config_AI["model_save"]:save_model(game.model, torch.optim.Adam(game.model.parameters(), lr=0.001),game.model_path)
+                if game.config.config_AI["genetic"]:save_model(game.model, torch.optim.Adam(game.model.parameters(), lr=0.001),game.model_path)
             case {"Player": True} | {"AI": True}:game.run_with_model()
         if game.exit:break
 pygame.quit(),sys.exit()
