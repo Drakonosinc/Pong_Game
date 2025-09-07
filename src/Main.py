4e0e0e7e45232617978c94ebae72a9c577f55665
matias <mat20can@gmail.com>
2025-09-07 16:16:51 -0300

    version 10.0

--- src/Main.py
+++ src/Main.py
@@ -7,7 +7,9 @@ if __name__=="__main__":
         game.game_over=False
         match game.mode_game:
             case {"Training AI": True}:
-                if game.config.config_AI["type_training"]["Genetic"]:best_model = genetic_algorithm(game, input_size=len(game.ai_handler.get_state()), output_size=2, generations=game.config.config_AI["generation_value"], population_size=game.config.config_AI["population_value"], num_trials=game.config.config_AI["try_for_ai"])
+                if game.config.config_AI["type_training"]["Genetic"]:
+                    gen_config = game.config.config_AI["genetic"]
+                    best_model = genetic_algorithm(game, input_size=len(game.ai_handler.get_state()), output_size=2, generations=game.config.config_AI["generation_value"], population_size=game.config.config_AI["population_value"], num_trials=game.config.config_AI["try_for_ai"])
                 elif game.config.config_AI["type_training"]["Q-learning"]:
                     q_config = game.config.config_AI["q_learning"]
                     best_model = q_learning_algorithm(game, input_size=len(game.ai_handler.get_state()), output_size=2, episodes=q_config["episodes"],lr=q_config["learning_rate"],gamma=q_config["gamma"],epsilon_start=q_config["epsilon_start"],epsilon_end=q_config["epsilon_end"],epsilon_decay=q_config["epsilon_decay"])
