794339101f07a7448f8008f5485a396c8c31d228
matias <mat20can@gmail.com>
2025-09-16 16:55:32 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -60,12 +60,8 @@ class ElementBehavior:
             self.execute_commands()
         if self.states["click_time"] is not None and not repeat:
             if current_time - self.states["click_time"] >= 200:
-                
-        if repeat and not pressed_mouse[0] and rect.collidepoint(mouse_pos) and self.states["presses_touch"]:
-            if self.sound_touch:self.sound_touch.play(loops=0)
-            self.states["click_time"] = None
-            self.states["presses_touch"] = True
-            self.execute_commands()
+                execute()
+        if repeat and not pressed_mouse[0] and rect.collidepoint(mouse_pos) and self.states["presses_touch"]:execute()
         if pressed_mouse[0] and not rect.collidepoint(mouse_pos):self.states["active"],self.states["presses_touch"]=False,True
         if self.states["active"]:self.draw_pressed_effect() if draw is None else draw()
     def draw_pressed_effect(self):return NotImplementedError
