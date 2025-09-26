75288dab825eadd3e4a319dbde3703c75e8a56e7
matias <mat20can@gmail.com>
2025-09-26 10:22:42 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -350,3 +350,4 @@ class ComboBoxDown(ComboBox):
         if not last_rect or first:y = self.position[1] + self.font.get_height() + i * (self.font.get_height() + 5)
         else:y = last_rect.bottom + 5
         return (self.position[0], y)
+    def _adapt_size_dropdown_specific(self) -> None: self.dropdown[1] = len(self.option_buttons) * (self.font.get_height() + 5)
