c4f47959873b430c25900b8b50a593338f0bf440
matias <mat20can@gmail.com>
2025-09-28 16:53:51 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -369,3 +369,4 @@ class ComboBoxUp(ComboBox):
     def _update_scroll_position(self) -> None:
         if self.dropdown_rect.height != self.dropdown[1]: self.scroll.rect["rect"].y = self.scroll.rect["thumb"].y = self.dropdown_rect.y
         self.scroll.rect["rect"].height = self.dropdown_rect.height
+class ComboBoxRight(ComboBox):
