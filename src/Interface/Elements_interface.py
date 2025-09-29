c9da1ce37cffd81248f75da2c29d996ec2cc107d
matias <mat20can@gmail.com>
2025-09-29 14:32:47 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -372,3 +372,6 @@ class ComboBoxUp(ComboBox):
 class ComboBoxRight(ComboBox):
     def get_icon_dropdown(self) -> str: return " >"
     def _get_anim_index(self) -> int:return 0
+    def _get_rect_dropdown(self) -> pygame.Rect:
+        self._anim_motion(self._get_anim_index())
+        return pygame.Rect(self.position[0] + (self.font.size(self.text)[0] + self.font.size(self.type_dropdown)[0]),self.position[1] + (self.font.get_height() / 2),self.anim_height_dropdown,self.dropdown[1])
