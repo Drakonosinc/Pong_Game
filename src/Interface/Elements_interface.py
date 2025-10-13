21e004c50e304262c5c776c780cd9c08b8a654ca
matias <mat20can@gmail.com>
2025-10-13 15:50:13 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -401,3 +401,4 @@ class ComboBoxLeft(ComboBox):
         return (x, self.position[1] + (self.font.get_height() / 2))
     def _adapt_size_dropdown_specific(self) -> None:
         if self.option_buttons:
+            keys = list(self.option_buttons.keys())
