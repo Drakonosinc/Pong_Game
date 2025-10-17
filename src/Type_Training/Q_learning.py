6ad6bb1ab2599e0b820533cddd21d781588db1e4
matias <mat20can@gmail.com>
2025-10-17 20:27:40 -0300

    version 10.0

--- src/Type_Training/Q_learning.py
+++ src/Type_Training/Q_learning.py
@@ -26,13 +26,11 @@ class DQNAgent:
         self.batch_size = batch_size
         self.steps_done = 0
         self.target_update = target_update
-        
         # Create networks - use SimpleNN for consistency with genetic algorithm
         self.policy_net = SimpleNN(state_size, action_size)
         self.target_net = SimpleNN(state_size, action_size)
         self.target_net.load_state_dict(self.policy_net.state_dict())
         self.target_net.eval()
-        
         self.optimizer = optim.Adam(self.policy_net.parameters(), lr=lr)
         self.memory = ReplayMemory(memory_size)
         
