36b60439f2f028f75cf7fc5ce9f77184e55c9700
matias <mat20can@gmail.com>
2025-10-17 20:28:22 -0300

    version 10.0

--- src/Type_Training/Q_learning.py
+++ src/Type_Training/Q_learning.py
@@ -39,9 +39,7 @@ class DQNAgent:
             tensor_state = torch.tensor(state, dtype=torch.float32)
             q_values = self.policy_net(tensor_state)
             return int(torch.argmax(q_values).item())
-            
-    def store_transition(self, state, action, reward, next_state, done):
-        self.memory.push((state, action, reward, next_state, done))
+    def store_transition(self, state, action, reward, next_state, done): self.memory.push((state, action, reward, next_state, done))
         
     def optimize_model(self):
         if len(self.memory) < self.batch_size:
