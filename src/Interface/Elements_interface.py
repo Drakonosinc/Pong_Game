741efb7a6863d5e3938d6fbd6fa0f8a7cf79c8f9
matias <mat20can@gmail.com>
2025-10-27 16:50:18 -0300

    version 10

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -314,3 +314,6 @@ class ComboBox(TextButton):
                 if isinstance(button.rect, pygame.Rect):button.rect.topleft = button.position
                 elif isinstance(button.rect, dict) and "button" in button.rect:button.rect["button"].topleft = button.position
             self._repeat_charge(f"buttons_{i}",button.text,button,i)
+    def _repeat_charge(self,key,text,button,i) -> None:
+        self.option_buttons[text] = button
+        self.rect[key] = button
