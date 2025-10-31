95d06942080fed3d8ad8d4f992eef7295fc915d9
matias <mat20can@gmail.com>
2025-10-31 18:31:29 -0300

    version 10

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -368,3 +368,4 @@ class ComboBoxUp(ComboBox):
         self.scroll.rect["rect"].height = self.dropdown_rect.height
     def _create_scroll(self) -> None:
         self.scroll = self.factory.create_ScrollBar({
+            "position": (self.position[0] + self.dropdown[0], self.position[1] + self.font.get_height(), 20, self.dropdown[1]),
