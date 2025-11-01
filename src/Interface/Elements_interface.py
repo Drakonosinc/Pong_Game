836f85be4544052465ffde1d12185ef6e17496fe
matias <mat20can@gmail.com>
2025-11-01 15:55:05 -0300

    version 10

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -381,3 +381,4 @@ class ComboBoxRight(ComboBox):
     def _adapt_size_dropdown(self) -> None:
         self.dropdown[0], self.dropdown[1], self.adapt_dropdown = (self.option_buttons[list(self.option_buttons.keys())[-1]].rect.right - self.option_buttons[list(self.option_buttons.keys())[0]].rect.left) + 10, (self.font.get_height() + 5), False
     def _check_buttons_position(self,i: int, text: str = "", first: bool = False) -> tuple[int, int]:
+        last_rect = self.option_buttons[list(self.option_buttons.keys())[-1]].rect if self.option_buttons else None
