42060e0b8ad3786db4b906320e0a0a4e2fe3fce2
matias <mat20can@gmail.com>
2025-11-04 15:59:38 -0300

    version 10

---
+++ src/Interface/Menus/Menu_AI.py
@@ -0,0 +1,6 @@
+from .Base_Menu import BaseMenu
+class AIMenu(BaseMenu):
+    def __init__(self, interface):
+        super().__init__(interface)
+        self.buttons = {}
+        self.config_buttons = {}
