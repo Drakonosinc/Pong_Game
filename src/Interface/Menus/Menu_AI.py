82b27ce0e4508d4fc680271b63767bd6a36a9fa5
matias <mat20can@gmail.com>
2025-11-04 16:00:03 -0300

    version 10

--- src/Interface/Menus/Menu_AI.py
+++ src/Interface/Menus/Menu_AI.py
@@ -4,3 +4,5 @@ class AIMenu(BaseMenu):
         super().__init__(interface)
         self.buttons = {}
         self.config_buttons = {}
+    def setup_buttons(self):
+        factory = self.interface.button_factory_f2_5
