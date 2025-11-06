86d8d3e0c5e94c9416c49e7eb251a1d9791bffdd
matias <mat20can@gmail.com>
2025-11-06 17:06:00 -0300

    version 10

--- src/Interface/Menus/Menu_AI.py
+++ src/Interface/Menus/Menu_AI.py
@@ -6,7 +6,7 @@ class AIMenu(BaseMenu):
         self.config_buttons = {}
     def setup_buttons(self):
         factory = self.interface.button_factory_f2_5
-        self.buttons['back'] = factory.create_PolygonButton({"position": ((50, 350), (50, 380), (25, 365)),"position2":((50, 340), (50, 390), (10, 365)),"command1":lambda:self.change_mains({"main":})})
+        self.buttons['back'] = factory.create_PolygonButton({"position": ((50, 350), (50, 380), (25, 365)),"position2":((50, 340), (50, 390), (10, 365)),"command1":lambda:self.change_mains({"main":2})})
     def render(self):
         self.screen.fill(self.interface.BLACK)
         self.execute_buttons(self.buttons['back'])
