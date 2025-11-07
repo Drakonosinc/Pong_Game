bab0c30cefac9a8052ceb5bb13035d0689cb7366
matias <mat20can@gmail.com>
2025-11-07 15:27:08 -0300

    version 10

--- src/Type_Model/Neural_Network_Pytorch.py
+++ src/Type_Model/Neural_Network_Pytorch.py
@@ -20,4 +20,4 @@ class SimpleNN(nn.Module):
             if i == 0:
                 with torch.no_grad():
                     act = x.detach().cpu().numpy().reshape(1, -1)
-                    if act.max() - act.min() != 0:
+                    if act.max() - act.min() != 0: self.activations = (act - act.min()) / (act.max() - act.min())
