886c4a148cc21fdf3ed75e56e362e6fb800bdc1c
matias <mat20can@gmail.com>
2025-11-07 15:19:51 -0300

    version 10

--- src/Main.py
+++ src/Main.py
@@ -9,11 +9,10 @@ if __name__=="__main__":
             case {"Training AI": True}:
                 if game.config.config_AI["type_training"]["Genetic"]:
                     genetic_config = game.config.config_AI["genetic"]
-                    best_model = genetic_algorithm(game, input_size=len(game.ai_handler.get_state()), output_size=2, generations=genetic_config["generation_value"], population_size=genetic_config["population_value"], num_trials=genetic_config["try_for_ai"])
+                    nn_cfg = game.config.config_AI.get("nn", {"hidden_layers": 2, "neurons_per_layer": 6})
                 elif game.config.config_AI["type_training"]["Q-learning"]:
                     q_config = game.config.config_AI["q_learning"]
-                    best_model = q_learning_algorithm(game, input_size=len(game.ai_handler.get_state()), output_size=2, episodes=q_config["episodes"],lr=q_config["learning_rate"],gamma=q_config["gamma"],epsilon_start=q_config["epsilon_start"],epsilon_end=q_config["epsilon_end"],epsilon_decay=q_config["epsilon_decay"])
-                game.model = best_model
+                    game.model = best_model
                 if game.config.config_AI["model_save"]:save_model(game.model, torch.optim.Adam(game.model.parameters(), lr=0.001),game.model_path)
             case {"Player": True} | {"AI": True}:game.run_with_model()
         if game.exit:break
