fba69e846f6b699938c0cb45a61fbe128ecef7df
matias <mat20can@gmail.com>
2025-11-07 15:20:16 -0300

    version 10

--- src/Main.py
+++ src/Main.py
@@ -11,8 +11,10 @@ if __name__=="__main__":
                     genetic_config = game.config.config_AI["genetic"]
                     nn_cfg = game.config.config_AI.get("nn", {"hidden_layers": 2, "neurons_per_layer": 6})
                     arch = [nn_cfg.get("neurons_per_layer", 6)] * nn_cfg.get("hidden_layers", 2)
+                    best_model = genetic_algorithm(game, input_size=len(game.ai_handler.get_state()), output_size=2, generations=genetic_config["generation_value"], population_size=genetic_config["population_value"], num_trials=genetic_config["try_for_ai"], hidden_sizes=arch)
                 elif game.config.config_AI["type_training"]["Q-learning"]:
                     q_config = game.config.config_AI["q_learning"]
+                    nn_cfg = game.config.config_AI.get("nn", {"hidden_layers": 2, "neurons_per_layer": 6})
                     game.model = best_model
                 if game.config.config_AI["model_save"]:save_model(game.model, torch.optim.Adam(game.model.parameters(), lr=0.001),game.model_path)
             case {"Player": True} | {"AI": True}:game.run_with_model()
