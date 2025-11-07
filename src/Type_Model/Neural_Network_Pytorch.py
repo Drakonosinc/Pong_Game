3016abec924c4d6cfa7cc4ae8feea3c875efcd48
matias <mat20can@gmail.com>
2025-11-07 15:24:21 -0300

    version 10

--- src/Type_Model/Neural_Network_Pytorch.py
+++ src/Type_Model/Neural_Network_Pytorch.py
@@ -5,3 +5,5 @@ import torch.nn.functional as F
 class SimpleNN(nn.Module):
     def __init__(self, input_size, output_size, hidden_sizes=None):
         super(SimpleNN, self).__init__()
+        if hidden_sizes is None or len(hidden_sizes) == 0:
+            hidden_sizes = [128]
