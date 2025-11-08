ea26472277ed46a704a6f2ecb9c3734909730d92
matias <mat20can@gmail.com>
2025-11-08 15:13:20 -0300

    version 10

--- src/Type_Model/Neural_Network_Tensorflow.py
+++ src/Type_Model/Neural_Network_Tensorflow.py
@@ -7,3 +7,4 @@ class SimpleNN(Model):
         if hidden_sizes is None or len(hidden_sizes) == 0: hidden_sizes = [128]
         self.hidden_layers = [layers.Dense(int(h), activation='relu') for h in hidden_sizes]
         self.output_layer = layers.Dense(output_size)
+        self.fc1 = self.hidden_layers[0]
