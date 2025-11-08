a29639163226a0a3b0588d6d784fa5fdf58d736f
matias <mat20can@gmail.com>
2025-11-08 15:12:21 -0300

    version 10

--- src/Type_Model/Neural_Network_Tensorflow.py
+++ src/Type_Model/Neural_Network_Tensorflow.py
@@ -3,3 +3,5 @@ import numpy as np
 
 class SimpleNN(Model):
     def __init__(self, input_size, output_size, hidden_sizes=None):
+        super(SimpleNN, self).__init__()
+        if hidden_sizes is None or len(hidden_sizes) == 0: hidden_sizes = [128]
