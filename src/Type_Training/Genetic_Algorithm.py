5ee4a6eb866f2ad5efc5d93fe77a2a05a4ce0457
matias <mat20can@gmail.com>
2025-11-09 16:04:55 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -16,3 +16,5 @@ def evaluate_population(population, game, num_trials=3):
     fitness_scores = []
     for model in population:
         score = [fitness_function(model, game) for _ in range(num_trials)]
+        fitness_scores.append(sum(score) / num_trials)
+    min_score = abs(min(fitness_scores)) if min(fitness_scores) < 0 else 0
