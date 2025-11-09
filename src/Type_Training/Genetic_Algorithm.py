14bf44a5c63b2467e089ccb79a91fc3788c43949
matias <mat20can@gmail.com>
2025-11-09 16:01:51 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -4,3 +4,6 @@ def fitness_function(model, game):
     game.model = model  
     score = game.run_with_model()
     return score
+
+def initialize_population(size, input_size, output_size, hidden_sizes=None):
+    population = []
