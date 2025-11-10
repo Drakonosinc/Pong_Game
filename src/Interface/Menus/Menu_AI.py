c154a52979ffd5cdd1288c44017882603a0a89d4
matias <mat20can@gmail.com>
2025-11-10 12:54:48 -0300

    version 10

--- src/Interface/Menus/Menu_AI.py
+++ src/Interface/Menus/Menu_AI.py
@@ -8,3 +8,4 @@ class AIMenu(BaseMenu):
         self.connections = []      # list[tuple[(x1,y1),(x2,y2)]]
         # Defaults if missing in config
         nn_cfg = self.interface.config.config_AI.setdefault("nn", {"hidden_layers": 2, "neurons_per_layer": 6})
+        nn_cfg["hidden_layers"] = max(1, int(nn_cfg.get("hidden_layers", 2)))
