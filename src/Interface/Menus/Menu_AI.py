491b4d49c6b12f1c8b846ffe77c133b63851e45f
matias <mat20can@gmail.com>
2025-11-10 12:54:16 -0300

    version 10

--- src/Interface/Menus/Menu_AI.py
+++ src/Interface/Menus/Menu_AI.py
@@ -1,12 +1,7 @@
+import pygame
 from .Base_Menu import BaseMenu
 class AIMenu(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
         self.buttons = {}
-        self.config_buttons = {}
-    def setup_buttons(self):
-        factory = self.interface.button_factory_f2_5
-        self.buttons['back'] = factory.create_PolygonButton({"position": ((50, 350), (50, 380), (25, 365)),"position2":((50, 340), (50, 390), (10, 365)),"command1":lambda:self.change_mains({"main":2})})
-    def render(self):
-        self.screen.fill(self.interface.BLACK)
-        self.execute_buttons(self.buttons['back'])
+        self.network_buttons = []  # list[list[button]] by layer (including input/output)
