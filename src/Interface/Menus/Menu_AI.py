0eaff11f556afdd02e8322b1075b87eee7802aa7
matias <mat20can@gmail.com>
2025-11-10 12:54:27 -0300

    version 10

--- src/Interface/Menus/Menu_AI.py
+++ src/Interface/Menus/Menu_AI.py
@@ -5,3 +5,4 @@ class AIMenu(BaseMenu):
         super().__init__(interface)
         self.buttons = {}
         self.network_buttons = []  # list[list[button]] by layer (including input/output)
+        self.connections = []      # list[tuple[(x1,y1),(x2,y2)]]
