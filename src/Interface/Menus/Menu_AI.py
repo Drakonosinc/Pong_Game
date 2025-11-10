c3770a239a655c3e2505f8f7bdfeba494b51c1b5
matias <mat20can@gmail.com>
2025-11-10 12:54:38 -0300

    version 10

--- src/Interface/Menus/Menu_AI.py
+++ src/Interface/Menus/Menu_AI.py
@@ -6,3 +6,5 @@ class AIMenu(BaseMenu):
         self.buttons = {}
         self.network_buttons = []  # list[list[button]] by layer (including input/output)
         self.connections = []      # list[tuple[(x1,y1),(x2,y2)]]
+        # Defaults if missing in config
+        nn_cfg = self.interface.config.config_AI.setdefault("nn", {"hidden_layers": 2, "neurons_per_layer": 6})
