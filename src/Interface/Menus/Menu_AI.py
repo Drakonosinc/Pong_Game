9988fc5baae4ca9b4f5d4e713a6bb96181b30e48
matias <mat20can@gmail.com>
2025-11-11 14:26:32 -0300

    version 10

--- src/Interface/Menus/Menu_AI.py
+++ src/Interface/Menus/Menu_AI.py
@@ -23,3 +23,6 @@ class AIMenu(BaseMenu):
         self.buttons['inc_layers'] = f_med.create_TextButton({"text": "+","position": (self.WIDTH//2-100, 60),"command1": lambda: self._change_layers(1)})
         self.buttons['dec_neurons'] = f_med.create_TextButton({"text": "-","position": (self.WIDTH//2+100, 60),"command1": lambda: self._change_neurons(-1)})
         self.buttons['inc_neurons'] = f_med.create_TextButton({"text": "+","position": (self.WIDTH//2+130, 60),"command1": lambda: self._change_neurons(1)})
+        # Build first visual network
+        self._rebuild_network_visual()
+ 
