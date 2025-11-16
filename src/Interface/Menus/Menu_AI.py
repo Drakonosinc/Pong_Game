2107f5b98ab98336cd0089b14aff9791a1ecdc60
matias <mat20can@gmail.com>
2025-11-16 15:49:18 -0300

    version 10

--- src/Interface/Menus/Menu_AI.py
+++ src/Interface/Menus/Menu_AI.py
@@ -81,3 +81,5 @@ class AIMenu(BaseMenu):
         # Current values
         cfg = self.interface.config.config_AI["nn"]
         self.screen.blit(self.interface.font5.render(str(cfg["hidden_layers"]), True, self.interface.WHITE), (self.WIDTH//2-115, 62))
+        self.screen.blit(self.interface.font5.render(str(cfg["neurons_per_layer"]), True, self.interface.WHITE), (self.WIDTH//2+85, 62))
+ 
