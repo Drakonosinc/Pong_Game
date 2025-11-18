ebabceab921b24bf39833059d14ccb80670b25d0
matias <mat20can@gmail.com>
2025-11-18 11:51:19 -0300

    version 10

--- src/Type_Model/Neural_Network_Tensorflow.py
+++ src/Type_Model/Neural_Network_Tensorflow.py
@@ -4,11 +4,9 @@ import numpy as np
 class SimpleNN(Model):
     def __init__(self, input_size, output_size, hidden_sizes=None):
         super(SimpleNN, self).__init__()
-        if hidden_sizes is None or len(hidden_sizes) == 0:
-            hidden_sizes = [128]
+        if hidden_sizes is None or len(hidden_sizes) == 0: hidden_sizes = [128]
         self.hidden_layers = [layers.Dense(int(h), activation='relu') for h in hidden_sizes]
         self.output_layer = layers.Dense(output_size)
-        # Backwards compatibility
         self.fc1 = self.hidden_layers[0]
         self.activations = None
     def forward(self, x):
