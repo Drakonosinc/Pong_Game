5ee16e6ffcf11f6a89b477e990385fccb5e9ba43
matias <mat20can@gmail.com>
2025-11-18 11:49:47 -0300

    version 10

--- src/Interface/Menus/Menu_AI.py
+++ src/Interface/Menus/Menu_AI.py
@@ -4,13 +4,11 @@ class AIMenu(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
         self.buttons = {}
-        self.network_buttons = []  # list[list[button]] by layer (including input/output)
-        self.connections = []      # list[tuple[(x1,y1),(x2,y2)]]
-        # Defaults if missing in config
+        self.network_buttons = []
+        self.connections = []
         nn_cfg = self.interface.config.config_AI.setdefault("nn", {"hidden_layers": 2, "neurons_per_layer": 6})
         nn_cfg["hidden_layers"] = max(1, int(nn_cfg.get("hidden_layers", 2)))
         nn_cfg["neurons_per_layer"] = max(1, int(nn_cfg.get("neurons_per_layer", 6)))
-        # Fixed IO sizes used by the current game
         self.input_size = 6
         self.output_size = 2
     def setup_buttons(self):
