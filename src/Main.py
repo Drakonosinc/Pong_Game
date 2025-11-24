4fc43fb3f9f177c39009da92728afdb0583fc0c9
matias <mat20can@gmail.com>
2025-11-24 17:18:44 -0300

    version 10

--- src/Main.py
+++ src/Main.py
@@ -16,7 +16,7 @@ if __name__=="__main__":
                 elif game.config.config_AI["type_training"]["Q-learning"]:
                     q_config = game.config.config_AI["q_learning"]
                     best_model = q_learning_algorithm(game, input_size=len(game.ai_handler.get_state()), output_size=2, episodes=q_config["episodes"],lr=q_config["learning_rate"],gamma=q_config["gamma"],epsilon_start=q_config["epsilon_start"],epsilon_end=q_config["epsilon_end"],epsilon_decay=q_config["epsilon_decay"], hidden_sizes=arch)
-                    # if
+                    if game.config.config_AI["model_save"]:save_qlearning_model(game.model, torch.optim.Adam(game.model.parameters(), lr=0.001),game.model_path)
                 game.model = best_model
             case {"Player": True} | {"AI": True}:game.run_with_model()
         if game.exit:break
