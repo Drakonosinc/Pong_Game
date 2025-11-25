735910b7cdf22ebffb99ba6b592d9d1608934ebe
matias <mat20can@gmail.com>
2025-11-25 15:20:37 -0300

    version 10

--- src/Main.py
+++ src/Main.py
@@ -9,6 +9,8 @@ if __name__=="__main__":
             case {"Training AI": True}:
                 nn_cfg = game.config.config_AI.get("nn", {"hidden_layers": 2, "neurons_per_layer": 6})
                 arch = [nn_cfg.get("neurons_per_layer", 6)] * nn_cfg.get("hidden_layers", 2)
+                # test
+                # type_model = next(k for k, v in game.config.config_AI["type_model"].items() if v)
                 if game.config.config_AI["type_model"]["Pytorch"]: type_model = "Pytorch"
                 elif game.config.config_AI["type_model"]["Tensorflow"]: type_model = "Tensorflow"
                 if game.config.config_AI["type_training"]["Genetic"]:
