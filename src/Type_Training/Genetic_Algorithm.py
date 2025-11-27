fcba3159e228996ecc875038794f623cd5913c8e
matias <mat20can@gmail.com>
2025-11-27 12:53:47 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -8,7 +8,9 @@ def fitness_function(model, game):
 def initialize_population(type_model, size, input_size, output_size, hidden_sizes=None):
     population = []
     for _ in range(size):
-        model = SimpleNN(input_size, output_size, hidden_sizes=hidden_sizes)
+        if type_model == "Pytorch":
+            model = SimpleNN_Pytorch(input_size, output_size, hidden_sizes=hidden_sizes)
+            
         population.append(model)
     return population
 
