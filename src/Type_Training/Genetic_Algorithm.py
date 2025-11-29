e0fae0264dbcee89bd54e54aa1b0906879ff5f99
matias <mat20can@gmail.com>
2025-11-29 19:36:15 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -3,8 +3,7 @@ import numpy as np
 from Type_Model import *
 import tensorflow as tf 
 
-def _is_torch_model(model):
-    return hasattr(model, 'parameters') and isinstance(model, torch.nn.Module)
+def _is_torch_model(model): return hasattr(model, 'parameters') and isinstance(model, torch.nn.Module)
 
 def _is_tf_model(model):
     return (tf is not None) and hasattr(model, 'trainable_variables') and isinstance(model, tf.keras.Model)
