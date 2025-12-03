a85c8fc8a87eac7e0b2ced1317ab30b91a06c5fd
matias <mat20can@gmail.com>
2025-12-03 14:40:59 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -51,3 +51,5 @@ def evaluate_population(population, game, num_trials=3):
     fitness_scores = []
     for model in population:
         score = [fitness_function(model, game) for _ in range(num_trials)]
+        fitness_scores.append(sum(score) / num_trials)
+ 
