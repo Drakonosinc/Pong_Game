526131cce782dc8ed7c6a6d9dc65361de9baa045
matias <mat20can@gmail.com>
2025-12-04 14:23:53 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -61,3 +61,4 @@ def select_parents(population, fitness_scores, num_parents):
     top_count = max(2, len(sorted_pop_fitness) // 2)
     top_population = [ind for ind, fit in sorted_pop_fitness[:top_count]]
     top_fitness = [fit for ind, fit in sorted_pop_fitness[:top_count]]
+    parents = random.choices(top_population, weights=top_fitness, k=num_parents)
