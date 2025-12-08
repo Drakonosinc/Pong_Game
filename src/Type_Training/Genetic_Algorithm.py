e234c62b70d5876dfe60d3de191f277473e2b795
matias <mat20can@gmail.com>
2025-12-08 15:53:27 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -103,3 +103,4 @@ def genetic_algorithm(game, type_model, input_size, output_size, generations=100
     best_fitness = -float('inf')
     best_model = None
     for generation in range(generations):
+        game.generation = generation
