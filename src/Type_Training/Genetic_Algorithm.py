83aa84e4266bed303fcd0a4ebc98f7ecad4c5737
matias <mat20can@gmail.com>
2025-12-09 15:50:52 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -116,3 +116,4 @@ def genetic_algorithm(game, type_model, input_size, output_size, generations=100
         sorted_population = [model for _, model in sorted(zip(fitness_scores, population), key=lambda x: x[0], reverse=True)]
         new_population = []
         for i in range(elitism):
+            elite_parent = sorted_population[i]
