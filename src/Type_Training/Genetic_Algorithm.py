81ebf3fbf6d28e2d208dfb7c1e84c78fda0630ae
matias <mat20can@gmail.com>
2025-12-10 16:17:22 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -124,3 +124,4 @@ def genetic_algorithm(game, type_model, input_size, output_size, generations=100
         parents = select_parents(population, fitness_scores, num_offsprings)
         offspring = []
         for i in range(0, len(parents) - 1, 2):
+            child1, child2 = crossover(parents[i], parents[i+1], type_model, input_size, output_size, hidden_sizes)
