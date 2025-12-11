ee2ad98cbcc8f3ab6afccbc988011b6ee8d15a46
matias <mat20can@gmail.com>
2025-12-11 16:00:44 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -138,3 +138,4 @@ def genetic_algorithm(game, type_model, input_size, output_size, generations=100
 
 def save_genetic_model(model, optimizer, path):
     print("save model")
+    if _is_torch_model(model): torch.save({'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict() if optimizer else {}}, path)
