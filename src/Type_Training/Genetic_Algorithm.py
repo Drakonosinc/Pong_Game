b8b0372342f474dcca15242c6d76d4531ae2d63e
matias <mat20can@gmail.com>
2025-12-14 16:34:25 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -178,4 +178,5 @@ def load_genetic_model(path, type_model, input_size, output_size, optimizer=None
             remapped = {}
             if 'fc1.weight' in state_dict: remapped['hidden_layers.0.weight'] = state_dict['fc1.weight']
             if 'fc1.bias' in state_dict:   remapped['hidden_layers.0.bias']   = state_dict['fc1.bias']
+            ow, ob = state_dict.get('fc2.weight'), state_dict.get('fc2.bias')
  
