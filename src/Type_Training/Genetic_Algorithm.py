9db9a7aa5cbcdb3741e2da7f72a47358e0445af3
matias <mat20can@gmail.com>
2025-12-15 17:03:20 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -181,4 +181,4 @@ def load_genetic_model(path, type_model, input_size, output_size, optimizer=None
             ow, ob = state_dict.get('fc2.weight'), state_dict.get('fc2.bias')
             if ow is not None and ob is not None:
                 if ow.shape[0] == output_size and ow.shape[1] == first_hidden and ob.shape[0] == output_size:
- 
+                    remapped['output_layer.weight'] = ow
