e701ea6b315c28544b633cc8b374d09de864d40e
matias <mat20can@gmail.com>
2025-12-15 17:04:58 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -189,4 +189,4 @@ def load_genetic_model(path, type_model, input_size, output_size, optimizer=None
             sizes = []
             for i in indices:
                 w_key = f'hidden_layers.{i}.weight'
- 
+                if w_key in state_dict: sizes.append(state_dict[w_key].shape[0])
