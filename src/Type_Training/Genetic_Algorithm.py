f3deaacd04c478ae6ae232649fe6340b083d7490
matias <mat20can@gmail.com>
2025-12-16 14:36:49 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -199,4 +199,4 @@ def load_genetic_model(path, type_model, input_size, output_size, optimizer=None
         if optimizer and 'optimizer_state_dict' in checkpoint: optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         return model
     except FileNotFoundError:
- 
+        print(f"The file {path} was not found.")
