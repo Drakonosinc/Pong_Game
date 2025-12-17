cdce8fba06916cf31397ae86bc1094aaa78f1fd9
matias <mat20can@gmail.com>
2025-12-17 18:58:41 -0300

    version 10

--- src/Events/InputHandler.py
+++ src/Events/InputHandler.py
@@ -8,3 +8,4 @@ class InputHandler:
             if event.type == QUIT: self.game.event_quit()
             self._process_keydown_events(event)
             if self.game.main == 2: self.game.events_buttons(event)
+            if self.game.main == 6: self.game.keys_menu.event_keys(event)
