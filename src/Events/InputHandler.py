41dd20d9ece8d4d9e61f024e4ac4b3c8cca1565e
matias <mat20can@gmail.com>
2025-12-18 17:13:30 -0300

    version 10

--- src/Events/InputHandler.py
+++ src/Events/InputHandler.py
@@ -19,3 +19,4 @@ class InputHandler:
             elif self.game.main == -1 and event.key == K_p: self.game.main = 3
             if self.game.main == 3 or self.game.main == -1:
                 if self.game.speed_up and event.key == K_KP_PLUS: self.game.change_speed(15, 1, 10, "speed_up", speed_up=self.game.speed_up)
+                if self.game.speed_down and event.key == K_KP_MINUS: self.game.change_speed(-15, -1, -1, "speed_down", speed_down=self.game.speed_down)
