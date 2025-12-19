4923d183002cc2419eecd5642ffdd98ef618ed2d
matias <mat20can@gmail.com>
2025-12-19 17:23:16 -0300

    version 10

--- src/Events/InputHandler.py
+++ src/Events/InputHandler.py
@@ -32,3 +32,4 @@ class InputHandler:
             p2 = self.game.player_two
             if keys[self.game.config.config_keys["UP_ARROW"]] and p2.rect.top > 0: p2.rect.y -= 5
             if keys[self.game.config.config_keys["DOWN_ARROW"]] and p2.rect.bottom < self.game.HEIGHT: p2.rect.y += 5
+        if self.game.main == 1:
