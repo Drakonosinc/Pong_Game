7f4f94867859b3b64ff543d4bb8aad2390c79422
matias <mat20can@gmail.com>
2025-12-20 16:55:33 -0300

    version 10

--- src/Game/Space_Pong.py
+++ src/Game/Space_Pong.py
@@ -181,7 +181,7 @@ class Space_pong_game(interface):
         self.running = True
         self.player_two.reward = 0
         while self.running and self.game_over==False:
-            , self.draw()
+            self.input_handler.handle_input(), self.draw()
             if self.main==-1:
                 if self.mode_game["Training AI"] or self.mode_game["AI"]: self.type_game()
                 self.update(), self.restart()
