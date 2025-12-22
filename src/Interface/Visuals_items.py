0233f2c1900b2580efc831222beb0b1921f25277
matias <mat20can@gmail.com>
2025-12-22 16:50:12 -0300

    version 10

--- src/Interface/Visuals_items.py
+++ src/Interface/Visuals_items.py
@@ -3,5 +3,5 @@ class Visuals_items:
     def __init__(self, game): self.game = game
     def mode_speed(self): self.game.screen.blit(self.game.font.render(f"Speed: {self.game.speed}", True, self.game.YELLOW),(self.game.WIDTH//2-40,360))
     def name_players(self):
-        self.screen.blit(self.font.render(f"{self.input_player1.show_player()}", True, self.YELLOW),(45,360))
-        self.screen.blit(self.font.render(f"{self.input_player2.show_player()}", True, self.YELLOW),(580,360))
+        self.game.screen.blit(self.game.font.render(f"{self.game.input_player1.show_player()}", True, self.game.YELLOW),(45,360))
+        self.game.screen.blit(self.game.font.render(f"{self.game.input_player2.show_player()}", True, self.game.YELLOW),(580,360))
