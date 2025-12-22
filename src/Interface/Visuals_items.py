610b9b093b3f6b81af0c2b06e0c3f146e4e30379
matias <mat20can@gmail.com>
2025-12-22 16:49:17 -0300

    version 10

--- src/Interface/Visuals_items.py
+++ src/Interface/Visuals_items.py
@@ -1,6 +1,5 @@
 import pygame
 class Visuals_items:
-    def __init__(self, game):
-        self.game = game
-    def mode_speed(self): self.screen.blit(self.font.render(f"Speed: {self.speed}", True, self.YELLOW),(self.WIDTH//2-40,360))
+    def __init__(self, game): self.game = game
+    def mode_speed(self): self.game.screen.blit(self.game.font.render(f"Speed: {self.game.speed}", True, self.game.YELLOW),(self.game.WIDTH//2-40,360))
     
