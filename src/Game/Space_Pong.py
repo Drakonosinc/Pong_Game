5924061a9f3b06a1db56f37905cfbd6c72366d97
matias <mat20can@gmail.com>
2025-12-22 16:51:07 -0300

    version 10

--- src/Game/Space_Pong.py
+++ src/Game/Space_Pong.py
@@ -105,7 +105,6 @@ class Space_pong_game(interface):
                 color_intensity = int(activation_value * 255)
                 color = (color_intensity, color_intensity, color_intensity)
                 pygame.draw.circle(self.screen, color, neuron_positions[i], 5)
-    def draw_generation(self):
     def draw_model_data(self):
         if self.mode_game["AI"]: self.model = self.model_training
         if self.model is not None:
