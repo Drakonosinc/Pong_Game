6edcb4c85bbb7b2fd49cdfdf72c8e745483c1f9d
matias <mat20can@gmail.com>
2025-12-22 16:49:40 -0300

    version 10

--- src/Game/Space_Pong.py
+++ src/Game/Space_Pong.py
@@ -127,8 +127,7 @@ class Space_pong_game(interface):
                     self.screen.blit(activations_text, (10, 70))
                 except Exception: pass
     def name_players(self):
-        self.screen.blit(self.font.render(f"{self.input_player1.show_player()}", True, self.YELLOW),(45,360))
-        self.screen.blit(self.font.render(f"{self.input_player2.show_player()}", True, self.YELLOW),(580,360))
+
     def reset(self,running=True, fps=60, speed=0, speed_up=True, speed_down=True):
         self.player_one.reset()
         self.player_two.reset()
