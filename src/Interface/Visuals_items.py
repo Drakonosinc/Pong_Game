e43a60ddb6beaeb714fd0bd4fb513dfc718e6590
matias <mat20can@gmail.com>
2025-12-26 16:11:35 -0300

    version 10

--- src/Interface/Visuals_items.py
+++ src/Interface/Visuals_items.py
@@ -35,3 +35,4 @@ class Visuals_items:
                 if hasattr(self.model, 'fc1'):
                     l = self.model.fc1
                     if hasattr(l, 'weight'): weights_preview = l.weight.detach().cpu().numpy().flatten()[:5]
+                    elif hasattr(l, 'kernel'): weights_preview = l.kernel.numpy().flatten()[:5]
