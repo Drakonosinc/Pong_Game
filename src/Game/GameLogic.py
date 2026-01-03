649b2592d2a79bcd01a6f3d94cbfc1305b8a7c43
matias <mat20can@gmail.com>
2026-01-03 18:10:58 -0300

    version 10

--- src/Game/GameLogic.py
+++ src/Game/GameLogic.py
@@ -9,3 +9,7 @@ class GameLogic:
         self.balls = [Ball(self.WIDTH//2-28,self.HEIGHT//2-29,36,36,4+i,4+i) for i in range(1 if self.mode_game["Training AI"] else self.config.config_game["number_balls"])]
         self.player_one = Player(25,150,11,90,[True] * len(self.balls))
         self.player_two = Player(665,150,11,90,[True] * len(self.balls))
+    def update(self):
+        for i, ball in enumerate(self.balls):
+            ball.move_ball(self.width, self.height)
+            
