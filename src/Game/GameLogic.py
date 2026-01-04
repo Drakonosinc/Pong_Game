0142c633b6656273030fb7182924148823170d98
matias <mat20can@gmail.com>
2026-01-04 16:43:46 -0300

    version 10

--- src/Game/GameLogic.py
+++ src/Game/GameLogic.py
@@ -23,4 +23,4 @@ class GameLogic:
         if player.check_collision(ball):
             if player.active[index]:
                 self._repeat_collision(ball, reward)
-                
+                player.active[index] = False
