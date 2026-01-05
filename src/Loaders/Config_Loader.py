155eb9421e295823ff152321d32076a7f975e691
matias <mat20can@gmail.com>
2026-01-05 17:00:32 -0300

    version 10

--- src/Loaders/Config_Loader.py
+++ src/Loaders/Config_Loader.py
@@ -4,3 +4,5 @@ class Config:
     def __init__(self):
         self.base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
         self.config_dir = os.path.join(self.base_dir, "Config")
+    def _read_json(self, filename: str) -> dict:
+        path = os.path.join(self.config_dir, filename)
