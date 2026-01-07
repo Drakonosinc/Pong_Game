ef63998a1d27646dabc71537c915aa48ed3dcc82
matias <mat20can@gmail.com>
2026-01-07 16:03:16 -0300

    version 10

--- src/Loaders/Config_Loader.py
+++ src/Loaders/Config_Loader.py
@@ -103,3 +103,8 @@ class Config:
                     "background1.jpg",
                     "background2.jpg",
                     "background3.jpg",
+                    "background4.jpg",
+                    "background5.jpg",
+                    "background6.jpg",
+                    "background7.jpg",
+                    "background8.jpg",],
