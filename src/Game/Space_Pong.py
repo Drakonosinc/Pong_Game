496376f50348f6603e1dcbd8d8c50e5fffaf6d85
matias <mat20can@gmail.com>
2026-01-11 15:39:35 -0300

    version 10

--- src/Game/Space_Pong.py
+++ src/Game/Space_Pong.py
@@ -60,7 +60,7 @@ class Space_pong_game(interface):
         self.running = running
         if hasattr(self, '_qlearning_state'): self.ai_handler.reset_qlearning_state()
     def type_game(self):
-        if self.mode_game["Training AI"]: self.player1_code()
+        if self.mode_game["Training AI"]: self.game_logic.auto_play_player1()
         self.ai_handler.actions_AI(self.model if self.mode_game["Training AI"] else self.model_training)
     def item_repeat_run(self):
         pygame.display.flip()
