47389a3801c4f8c235b72950ce67589ec823764e
matias <mat20can@gmail.com>
2026-01-11 15:38:53 -0300

    version 10

--- src/Game/GameLogic.py
+++ src/Game/GameLogic.py
@@ -32,3 +32,6 @@ class GameLogic:
         self.player_one.reset()
         self.player_two.reset()
         for ball in self.balls: ball.reset()
+    def auto_play_player1(self):
+        p1 = self.player_one
+        
