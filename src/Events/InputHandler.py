88e322564e5583ce2ee27d2afbfba95fe76d1a36
matias <mat20can@gmail.com>
2026-01-12 13:51:27 -0300

    version 10

--- src/Events/InputHandler.py
+++ src/Events/InputHandler.py
@@ -20,7 +20,7 @@ class InputHandler:
             if self.game.main == 3 or self.game.main == -1:
                 if self.game.speed_up and event.key == K_KP_PLUS: self.game.change_speed(15, 1, 10, "speed_up", speed_up=self.game.speed_up)
                 if self.game.speed_down and event.key == K_KP_MINUS: self.game.change_speed(-15, -1, -1, "speed_down", speed_down=self.game.speed_down)
-            if self.game.main == -1 and event.key == K_1: self.game.manual_save_model()
+            if self.game.main == -1 and event.key == K_1: self.game.ai_handler.manual_save_model()
     def _process_continuous_presses(self):
         keys = self.game.pressed_keys
         if keys[K_ESCAPE]: self.game.running = False
