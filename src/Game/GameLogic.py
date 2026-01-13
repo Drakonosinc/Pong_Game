452080b54737bae13e6f646ef97030ef96bbce6f
matias <mat20can@gmail.com>
2026-01-13 15:50:29 -0300

    version 10

--- src/Game/GameLogic.py
+++ src/Game/GameLogic.py
@@ -3,6 +3,7 @@ from Entities import *
 class GameLogic:
     def __init__(self, game):
         self.game = game
+        self.width = game.WIDTH
         self.init_entities()
     def init_entities(self):
         self.balls = [Ball(self.game.WIDTH//2-28,self.game.HEIGHT//2-29,36,36,4+i,4+i) for i in range(1 if self.game.mode_game["Training AI"] else self.game.config.config_game["number_balls"])]
