f8759a2afc3dc658f65b39f7f68d14e19e1c5a20
matias <mat20can@gmail.com>
2026-01-15 14:15:17 -0300

    version 10

--- src/Core/WindowManager.py
+++ src/Core/WindowManager.py
@@ -4,3 +4,5 @@ class WindowManager:
     def __init__(self, title="Space Pong AI", width=1000, height=600):
         self.window_width = width
         self.window_height = height
+        self.render_width = width   
+        self.render_height = height
