2ddaa0463e6ad60357351a701c6dbe52578fcd92
matias <mat20can@gmail.com>
2026-01-15 14:15:09 -0300

    version 10

--- src/Core/WindowManager.py
+++ src/Core/WindowManager.py
@@ -1,3 +1,6 @@
 import pygame
 from pygame.locals import *
-
+class WindowManager:
+    def __init__(self, title="Space Pong AI", width=1000, height=600):
+        self.window_width = width
+        self.window_height = height
