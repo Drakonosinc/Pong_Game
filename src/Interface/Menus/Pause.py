3786f573dcb5e2ea612e732ef22b69b8e53f5233
matias <mat20can@gmail.com>
2026-01-20 14:10:40 -0300

    version 10

--- src/Interface/Menus/Pause.py
+++ src/Interface/Menus/Pause.py
@@ -1,4 +1,5 @@
 from .Base_Menu import BaseMenu
+from Utils.States import GameState
 class Pause(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
@@ -6,8 +7,8 @@ class Pause(BaseMenu):
     def setup_buttons(self):
         factory = self.interface.button_factory_f2_5
         self.buttons['exit'] = factory.create_TextButton({"text": "Exit","color": self.interface.GRAY,"position": (self.WIDTH/2-40, self.HEIGHT/2-15),"color2": self.interface.SKYBLUE,"sound_touch": None,"command1": self.interface.event_quit})
-        self.buttons['reset'] = factory.create_TextButton({"text": "Reset","color": self.interface.GRAY,"position": (self.WIDTH/2-55, self.HEIGHT/2-85),"color2": self.interface.SKYBLUE,"command1": lambda: self.change_mains({"main": -1}),"command2": self.interface.reset})
-        self.buttons['menu'] = factory.create_TextButton({"text": "Menu","color": self.interface.GRAY,"position": (self.WIDTH/2-45, self.HEIGHT/2-50),"color2": self.interface.SKYBLUE,"command1": lambda: self.change_mains({"main": 0, "run": True}),"command2": self.interface.reset})
+        self.buttons['reset'] = factory.create_TextButton({"text": "Reset","color": self.interface.GRAY,"position": (self.WIDTH/2-55, self.HEIGHT/2-85),"color2": self.interface.SKYBLUE,"command1": lambda: self.change_mains({"main": GameState.PLAYING}),"command2": self.interface.reset})
+        self.buttons['menu'] = factory.create_TextButton({"text": "Menu","color": self.interface.GRAY,"position": (self.WIDTH/2-45, self.HEIGHT/2-50),"color2": self.interface.SKYBLUE,"command1": lambda: self.change_mains({"main": GameState.MENU, "run": True}),"command2": self.interface.reset})
         self.interface.exit_button = self.buttons['exit']
         self.interface.reset_pause_button = self.buttons['reset']
         self.interface.go_main_button = self.buttons['menu']
