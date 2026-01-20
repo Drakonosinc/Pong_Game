dc15d4a49b292266ff40757bc0cb7219330ad480
matias <mat20can@gmail.com>
2026-01-20 14:12:23 -0300

    version 10

--- src/Interface/Menus/Options_Menu.py
+++ src/Interface/Menus/Options_Menu.py
@@ -1,4 +1,5 @@
 from .Base_Menu import BaseMenu
+from Utils.States import GameState
 class OptionsMenu(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
@@ -6,10 +7,10 @@ class OptionsMenu(BaseMenu):
     def setup_buttons(self):
         factory_f5 = self.interface.button_factory_f5
         factory_f2_5 = self.interface.button_factory_f2_5
-        self.buttons['back'] = factory_f5.create_PolygonButton({"position": ((50, 350), (50, 380), (25, 365)),"position2": ((50, 340), (50, 390), (10, 365)),"command1": lambda: self.change_mains({"main": 0})})
-        self.buttons['visual'] = factory_f2_5.create_TextButton({"text": "Visuals","position": (self.WIDTH/2-80, self.HEIGHT/2-150),"command1": lambda: self.change_mains({"main": 5})})
+        self.buttons['back'] = factory_f5.create_PolygonButton({"position": ((50, 350), (50, 380), (25, 365)),"position2": ((50, 340), (50, 390), (10, 365)),"command1": lambda: self.change_mains({"main": GameState.MENU})})
+        self.buttons['visual'] = factory_f2_5.create_TextButton({"text": "Visuals","position": (self.WIDTH/2-80, self.HEIGHT/2-150),"command1": lambda: self.change_mains({"main": GameState.VISUALS})})
         self.buttons['sound'] = factory_f2_5.create_TextButton({"text": self.interface.sound_type["sound"],"color": self.interface.sound_type["color"],"position": (self.WIDTH/2-80, self.HEIGHT/2-115),"command1": lambda: self.on_off(self.config.config_sounds, "sound_main"),"command2": self._toggle_sound,"command3": self.config.save_config})
-        self.buttons['keys'] = factory_f2_5.create_TextButton({"text": "Keys","position": (self.WIDTH/2-80, self.HEIGHT/2-80),"command1": lambda: self.change_mains({"main": 6})})
+        self.buttons['keys'] = factory_f2_5.create_TextButton({"text": "Keys","position": (self.WIDTH/2-80, self.HEIGHT/2-80),"command1": lambda: self.change_mains({"main": GameState.KEYS})})
         self.buttons['language'] = factory_f2_5.create_TextButton({"text": "Language","position": (self.WIDTH/2-80, self.HEIGHT/2-45)})
         self.interface.back_button = self.buttons['back']
         self.interface.visual_button = self.buttons['visual']
