563d349614f173c3555d7df537804c3aba23fefd
matias <mat20can@gmail.com>
2026-01-21 14:44:44 -0300

    version 10

--- src/Interface/Menus/Main_Menu.py
+++ src/Interface/Menus/Main_Menu.py
@@ -6,9 +6,9 @@ class MainMenu(BaseMenu):
         self.buttons = {}
     def setup_buttons(self):
         factory = self.interface.button_factory_f5
-        self.buttons['play'] = factory.create_TextButton({"text": "Press To Start","position": (self.WIDTH//2-200,self.HEIGHT//2-80),"command1":lambda:self.change_mains({"main":2})})
+        self.buttons['play'] = factory.create_TextButton({"text": "Press To Start","position": (self.WIDTH//2-200,self.HEIGHT//2-80),"command1":lambda:self.change_mains({"main":GameState.MODE_SELECT})})
         self.buttons['quit'] = factory.create_TextButton({"text": "Press To Exit","position": (self.WIDTH//2-200,self.HEIGHT//2-50),"sound_touch":None,"command1": self.interface.event_quit})
-        self.buttons['options'] = factory.create_TextButton({"text": "Options","position": (self.WIDTH-110,self.HEIGHT-40),"command1":lambda:self.change_mains({"main":4})})
+        self.buttons['options'] = factory.create_TextButton({"text": "Options","position": (self.WIDTH-110,self.HEIGHT-40),"command1":lambda:self.change_mains({"main":GameState.OPTIONS})})
         self.interface.play_button = self.buttons['play']
         self.interface.quit_button = self.buttons['quit']
         self.interface.options_button = self.buttons['options']
