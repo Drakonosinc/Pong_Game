99814b01c7d8928160adc5f363885e82996abca7
matias <mat20can@gmail.com>
2026-01-22 13:49:54 -0300

    version 10

--- src/Events/EventManager.py
+++ src/Events/EventManager.py
@@ -5,3 +5,5 @@ class EventManager:
     def __init__(self): self.listeners: Dict[Type[Event], List[Callable]] = defaultdict(list)
     def subscribe(self, event_type: Type[Event], listener: Callable):
         self.listeners[event_type].append(listener)
+    def unsubscribe(self, event_type: Type[Event], listener: Callable):
+        if event_type in self.listeners:
