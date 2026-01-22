ac653f0ba09b44e3ecfa5dd7fee670a8f9885111
matias <mat20can@gmail.com>
2026-01-22 13:49:44 -0300

    version 10

--- src/Events/EventManager.py
+++ src/Events/EventManager.py
@@ -1,3 +1,7 @@
 from typing import Callable, Type, Dict, List
 from collections import defaultdict
 class Event: pass
+class EventManager:
+    def __init__(self): self.listeners: Dict[Type[Event], List[Callable]] = defaultdict(list)
+    def subscribe(self, event_type: Type[Event], listener: Callable):
+        self.listeners[event_type].append(listener)
