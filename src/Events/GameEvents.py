951e205a72fc5dd91009ae70b9eeef3477388a62
matias <mat20can@gmail.com>
2026-01-22 13:53:44 -0300

    version 10

--- src/Events/GameEvents.py
+++ src/Events/GameEvents.py
@@ -6,3 +6,6 @@ class ResumeGameEvent(Event): pass
 class ChangeStateEvent(Event):
     def __init__(self, new_state_data: dict): self.new_state_data = new_state_data
 class SaveModelEvent(Event): pass
+class PlayerMoveEvent(Event):
+    def __init__(self, player_index: int, direction: int, dt: float = 1.0):
+        self.player_index = player_index
