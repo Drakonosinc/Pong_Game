a2cb3a43e99e7f9a508bdd8c119bceaa8a4b380b
matias <mat20can@gmail.com>
2026-01-22 13:53:28 -0300

    version 10

--- src/Events/GameEvents.py
+++ src/Events/GameEvents.py
@@ -3,3 +3,5 @@ class QuitEvent(Event): pass
 class ToggleFullscreenEvent(Event): pass
 class PauseGameEvent(Event): pass
 class ResumeGameEvent(Event): pass
+class ChangeStateEvent(Event):
+    def __init__(self, new_state_data: dict): self.new_state_data = new_state_data
