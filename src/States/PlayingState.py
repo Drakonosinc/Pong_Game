9af1186bd2b18a76451ceb7fbb09adccaaf42dc6
matias <mat20can@gmail.com>
2026-01-24 15:32:31 -0300

    version 10

--- src/States/PlayingState.py
+++ src/States/PlayingState.py
@@ -1,2 +1,9 @@
 from .State import State
 from Utils.States import GameState
+class PlayingState(State):
+    def __init__(self, game):
+        super().__init__(game)
+    def enter(self, params=None):
+        self.game.main = GameState.PLAYING
+        self.game.game_over = False
+        if params and params.get("reset"): self.game.reset()
