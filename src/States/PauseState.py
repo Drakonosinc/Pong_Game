a3b22f5239d4d242d6789a30b577c20f5915c44f
matias <mat20can@gmail.com>
2026-01-26 16:25:02 -0300

    version 10

---
+++ src/States/PauseState.py
@@ -0,0 +1,9 @@
+from .State import State
+from Utils.States import GameState
+class PauseState(State):
+    def enter(self, params=None):
+        self.game.main = GameState.PAUSE
+    def exit(self): pass
+    def update(self, dt): pass
+    def draw(self, surface): self.game.visuals_items.draw()
+    def handle_event(self, event): pass
