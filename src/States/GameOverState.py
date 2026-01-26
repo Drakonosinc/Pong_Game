84bbd67a84eec943ece1bff615cee7dab047c292
matias <mat20can@gmail.com>
2026-01-26 16:23:58 -0300

    version 10

--- src/States/GameOverState.py
+++ src/States/GameOverState.py
@@ -2,3 +2,8 @@ from .State import State
 from Utils.States import GameState
 class GameOverState(State):
     def enter(self, params=None):
+        self.game.main = GameState.GAME_OVER
+    def exit(self): pass
+    def update(self, dt): pass
+    def draw(self, surface): self.game.visuals_items.draw()
+    def handle_event(self, event): pass
