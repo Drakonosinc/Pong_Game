63384a67cc73b940c281a4e88a8663a352c0e3e8
matias <mat20can@gmail.com>
2026-01-27 15:53:30 -0300

    version 10

--- src/States/VisualsState.py
+++ src/States/VisualsState.py
@@ -4,3 +4,5 @@ class VisualsState(State):
     def enter(self, params=None):
         self.game.main = GameState.VISUALS
     def exit(self): pass
+    def update(self, dt): pass
+    def draw(self, surface): self.game.visuals_items.draw()
