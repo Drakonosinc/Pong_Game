b784d22679574ff11db937acb9d9c7534b69e5b9
matias <mat20can@gmail.com>
2026-01-30 15:27:53 -0300

    version 10

--- src/Core/GameContext.py
+++ src/Core/GameContext.py
@@ -2,3 +2,6 @@ class GameContext:
     def __init__(self, config, assets, event_manager, window_manager, sound_manager=None):
         self.config = config
         self.assets = assets
+        self.event_manager = event_manager
+        self.window_manager = window_manager
+        self.sound = sound_manager
