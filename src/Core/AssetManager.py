d6309ed60381977b346c9bd152b9ade22597c0ef
matias <mat20can@gmail.com>
2026-01-31 16:38:31 -0300

    version 10

--- src/Core/AssetManager.py
+++ src/Core/AssetManager.py
@@ -20,3 +20,7 @@ class AssetManager:
         self.GREEN = (0, 255, 0)
         self.BLUE = (0, 0, 255)
         self.SKYBLUE = (135, 206, 235)
+        self.YELLOW = (255, 255, 0)
+        self.RED = (255, 0, 0)
+        self.GOLDEN = (255, 199, 51)
+        self.background_color = self.GRAY
