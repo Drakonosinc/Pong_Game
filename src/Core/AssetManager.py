15eb79cbdf758481f5d72925d94f6be529452653
matias <mat20can@gmail.com>
2026-01-31 16:37:08 -0300

    version 10

--- src/Core/AssetManager.py
+++ src/Core/AssetManager.py
@@ -3,3 +3,6 @@ import os
 class AssetManager:
     def __init__(self, config, window_manager):
         self.config = config
+        self.window_manager = window_manager
+        self.WIDTH = window_manager.render_width
+        self.HEIGHT = window_manager.render_height
