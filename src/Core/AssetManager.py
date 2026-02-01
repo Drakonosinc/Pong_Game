bcbffb01ae2f2497f8e83363d5a24dc0ca4d0982
matias <mat20can@gmail.com>
2026-02-01 16:55:38 -0300

    version 10

--- src/Core/AssetManager.py
+++ src/Core/AssetManager.py
@@ -44,3 +44,5 @@ class AssetManager:
             self.font5 = self.font
     def load_sounds(self):
         self.sound = pygame.mixer.Sound(os.path.join(self.sound_path, "pong.wav"))
+        self.sound_touchletters = pygame.mixer.Sound(os.path.join(self.sound_path, "touchletters.wav"))
+        self.sound_exitbutton = pygame.mixer.Sound(os.path.join(self.sound_path, "exitbutton.wav"))
