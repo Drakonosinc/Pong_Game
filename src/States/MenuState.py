1c38ab0e92a66a1bae910047ddec2449ba9fc77f
matias <mat20can@gmail.com>
2026-02-03 11:28:37 -0300

    version 10

--- src/States/MenuState.py
+++ src/States/MenuState.py
@@ -1,11 +1,17 @@
 from .State import State
 from Utils.States import GameState
+from Interface.Menus.Main_Menu import MainMenu
 class MenuState(State):
     def __init__(self, game):
         super().__init__(game)
-    def enter(self, params=None): self.game.main = GameState.MENU
-    def exit(self): pass
-    def update(self, dt): pass
-    def draw(self, surface): self.game.visuals_items.draw()
+        self.menu = MainMenu(self.game.ui)
+    def enter(self, params=None):
+        self.game.main = GameState.MENU
+        self.game.ui.setup_button_factories()
+        self.menu.setup_buttons()
+    def exit(self):pass
+    def update(self, dt):pass
+    def draw(self, surface):
+        self.menu.render()
     def handle_event(self, event): 
-        if self.game.main == GameState.MODE_SELECT: self.game.events_buttons(event)
+        if self.game.main == GameState.MENU: self.game.events_buttons(event)
