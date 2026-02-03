e75e734859e9ff8262fbbb8a44d5f78fa7f96227
matias <mat20can@gmail.com>
2026-02-03 11:26:45 -0300

    version 10

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -1,6 +1,5 @@
 from .Elements_interface import *
-from .Menus import *
-from Utils.States import GameState
+from Interface.Menus.Base_Menu import BaseMenu 
 class Interface(BaseMenu):
     def __init__(self, context):
         BaseMenu.__init__(self, self)
@@ -25,27 +24,7 @@ class Interface(BaseMenu):
         self.spacecraft2 = assets.spacecraft2
         self.SKYBLUE = assets.SKYBLUE
         self.RED = assets.RED
-        self.initialize_menus()
-    def initialize_menus(self):
-        self.main_menu = MainMenu(self)
-        self.game_over_menu = GameOver(self)
-        self.game_mode_menu = GameMode(self)
-        self.pause_menu = Pause(self)
-        self.options_menu = OptionsMenu(self)
-        self.visuals_menu = VisualsMenu(self)
-        self.keys_menu = KeysMenu(self)
-        self.menu_AI = AIMenu(self)
-    def menus(self, current_state_enum):
-        menu_routes = {
-            GameState.MENU: self.main_menu.render,
-            GameState.GAME_OVER: self.game_over_menu.render,
-            GameState.MODE_SELECT: self.game_mode_menu.render,
-            GameState.PAUSE: self.pause_menu.render,
-            GameState.OPTIONS: self.options_menu.render,
-            GameState.VISUALS: self.visuals_menu.render,
-            GameState.KEYS: self.keys_menu.render,
-            GameState.AI_MENU: self.menu_AI.render }
-        if current_state_enum in menu_routes: menu_routes[current_state_enum]()
+        self.setup_button_factories()
     def setup_button_factories(self):
         self.button_factory_f5 = ElementsFactory({
             "screen": self.screen, 
@@ -59,16 +38,6 @@ class Interface(BaseMenu):
             "font": self.font2_5,
             "sound_hover": self.sound_buttonletters,
             "sound_touch": self.sound_touchletters})
-    def draw_buttons(self):
-        self.setup_button_factories()
-        self.main_menu.setup_buttons()
-        self.game_over_menu.setup_buttons()
-        self.game_mode_menu.setup_buttons()
-        self.pause_menu.setup_buttons()
-        self.options_menu.setup_buttons()
-        self.visuals_menu.setup_buttons()
-        self.keys_menu.setup_buttons()
-        self.menu_AI.setup_buttons()
     def events_buttons(self, event):
         if hasattr(self, 'decrease_score_button'): self.decrease_score_button.reactivate_pressed(event)
         if hasattr(self, 'increase_score_button'): self.increase_score_button.reactivate_pressed(event)
