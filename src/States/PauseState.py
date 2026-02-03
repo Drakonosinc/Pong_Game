cadcb05155bb31dd5fb2924ae9bd02c3675e9243
matias <mat20can@gmail.com>
2026-02-03 11:34:16 -0300

    version 10

--- src/States/PauseState.py
+++ src/States/PauseState.py
@@ -1,9 +1,18 @@
 from .State import State
 from Utils.States import GameState
+from Interface.Menus.Pause import Pause
 class PauseState(State):
+    def __init__(self, game):
+        super().__init__(game)
+        self.menu = Pause(self.game.ui)
     def enter(self, params=None):
         self.game.main = GameState.PAUSE
+        self.game.ui.setup_button_factories()
+        self.menu.setup_buttons()
     def exit(self): pass
     def update(self, dt): pass
-    def draw(self, surface): self.game.visuals_items.draw()
-    def handle_event(self, event): pass
+    def draw(self, surface):
+        self.game.visuals_items.draw()
+        self.menu.render()
+    def handle_event(self, event):
+        if self.game.main == GameState.PAUSE: self.game.events_buttons(event)
