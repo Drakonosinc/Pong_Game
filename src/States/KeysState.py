bfb3f72299b59283c5aa50fdf414862a41976b52
matias <mat20can@gmail.com>
2026-02-04 16:45:34 -0300

    version 10

--- src/States/KeysState.py
+++ src/States/KeysState.py
@@ -1,9 +1,7 @@
 from .State import State
 from Utils.States import GameState
+from Interface.Menus.Menu_Keys import KeysMenu
 class KeysState(State):
-    def enter(self, params=None):
-        self.game.main = GameState.KEYS
-    def exit(self): pass
-    def update(self, dt): pass
-    def draw(self, surface): self.game.visuals_items.draw()
-    def handle_event(self, event): self.game.keys_menu.event_keys(event)
+    def __init__(self, game):
+        super().__init__(game)
+        self.menu = KeysMenu(self.game.ui)
