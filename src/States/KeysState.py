50e968966cb0ffffedc33b644d294b717eaf7e8e
matias <mat20can@gmail.com>
2026-02-04 16:45:47 -0300

    version 10

--- src/States/KeysState.py
+++ src/States/KeysState.py
@@ -5,3 +5,7 @@ class KeysState(State):
     def __init__(self, game):
         super().__init__(game)
         self.menu = KeysMenu(self.game.ui)
+    def enter(self, params=None):
+        self.game.main = GameState.KEYS
+        self.game.ui.setup_button_factories()
+        self.menu.setup_buttons()
