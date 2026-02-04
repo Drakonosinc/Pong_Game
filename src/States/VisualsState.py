793e03e32d00f825932e55ad50bc0ac4221d323b
matias <mat20can@gmail.com>
2026-02-04 16:42:54 -0300

    version 10

--- src/States/VisualsState.py
+++
@@ -1,9 +0,0 @@
-from .State import State
-from Utils.States import GameState
-class VisualsState(State):
-    def enter(self, params=None):
-        self.game.main = GameState.VISUALS
-    def exit(self): pass
-    def update(self, dt): pass
-    def draw(self, surface): self.game.visuals_items.draw()
-    def handle_event(self, event): pass
