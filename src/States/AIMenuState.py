a179d2b4d1e44f9c1dfb5c03e76afa8cac43a627
matias <mat20can@gmail.com>
2026-02-05 13:59:23 -0300

    version 10

--- src/States/AIMenuState.py
+++ src/States/AIMenuState.py
@@ -1,3 +1,7 @@
 from .State import State
 from Utils.States import GameState
 from Interface.Menus.Menu_AI import AIMenu
+class AIMenuState(State):
+    def __init__(self, game):
+        super().__init__(game)
+        self.menu = AIMenu(self.game.ui)
