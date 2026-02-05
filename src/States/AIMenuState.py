0ee819652f591c6815a5d5d41e95af145ebcb2fa
matias <mat20can@gmail.com>
2026-02-05 13:58:56 -0300

    version 10

--- src/States/AIMenuState.py
+++
@@ -1,9 +0,0 @@
-from .State import State
-from Utils.States import GameState
-class AIMenuState(State):
-    def enter(self, params=None):
-        self.game.main = GameState.AI_MENU
-    def exit(self): pass
-    def update(self, dt): pass
-    def draw(self, surface): self.game.visuals_items.draw()
-    def handle_event(self, event): pass
