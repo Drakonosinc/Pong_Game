abc802c0c5e918d6789db1ab0a8c33b000397c87
matias <mat20can@gmail.com>
2026-02-05 13:59:32 -0300

    version 10

--- src/States/AIMenuState.py
+++ src/States/AIMenuState.py
@@ -5,3 +5,7 @@ class AIMenuState(State):
     def __init__(self, game):
         super().__init__(game)
         self.menu = AIMenu(self.game.ui)
+    def enter(self, params=None):
+        self.game.main = GameState.AI_MENU
+        self.game.ui.setup_button_factories()
+        self.menu.setup_buttons()
