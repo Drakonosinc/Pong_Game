6ddf9319f5f2f3a0b5c7245f627f946ce6cc7bef
matias <mat20can@gmail.com>
2026-02-06 15:11:23 -0300

    version 10

--- src/States/GameOverState.py
+++ src/States/GameOverState.py
@@ -1,16 +1,22 @@
 from .State import State
 from Utils.States import GameState
 from Interface.Menus.Game_Over import GameOver
+from .MenuState import MenuState
+from .PlayingState import PlayingState
 class GameOverState(State):
     def __init__(self, game):
         super().__init__(game)
         self.menu = GameOver(self.game.ui)
     def enter(self, params=None):
         self.game.main = GameState.GAME_OVER
+        self.game.ui.main = GameState.GAME_OVER
         self.game.ui.setup_button_factories()
         self.menu.setup_buttons()
     def exit(self): pass
-    def update(self, dt): pass
+    def update(self, dt):
+        ui_main = getattr(self.game.ui, 'main', None)
+        if ui_main == GameState.MENU: self.game.state_manager.change(MenuState(self.game))
+        elif ui_main == GameState.PLAYING: self.game.state_manager.change(PlayingState(self.game))
     def draw(self, surface):
         self.game.visuals_items.draw()
         self.menu.render()
