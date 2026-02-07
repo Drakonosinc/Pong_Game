fe064592c6633859e21ec764f7e73eb6dbcc8d56
matias <mat20can@gmail.com>
2026-02-07 13:37:24 -0300

    version 10

--- src/States/OptionsState.py
+++ src/States/OptionsState.py
@@ -1,18 +1,6 @@
 from .State import State
 from Utils.States import GameState
 from Interface.Menus.Options_Menu import OptionsMenu
-class OptionsState(State):
-    def __init__(self, game):
-        super().__init__(game)
-        self.menu = OptionsMenu(self.game.ui)
-    def enter(self, params=None):
-        self.game.main = GameState.OPTIONS
-        self.game.ui.setup_button_factories()
-        self.menu.setup_buttons()
-    def exit(self): pass
-    def update(self, dt): pass
-    def draw(self, surface):
-        self.game.visuals_items.draw()
-        self.menu.render()
-    def handle_event(self, event):
-        if self.game.main == GameState.OPTIONS: self.game.events_buttons(event)
+from .VisualsState import VisualsState
+from .KeysState import KeysState
+from .MenuState import MenuState
