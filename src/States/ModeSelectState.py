4f94e91f69425e92788e53dd4b7147cc551e815d
matias <mat20can@gmail.com>
2026-02-07 13:35:30 -0300

    version 10

--- src/States/ModeSelectState.py
+++ src/States/ModeSelectState.py
@@ -4,3 +4,7 @@ from Interface.Menus.Game_Mode import GameMode
 from .PlayingState import PlayingState
 from .AIMenuState import AIMenuState
 from .MenuState import MenuState
+class ModeSelectState(State):
+    def __init__(self, game):
+        super().__init__(game)
+        self.menu = GameMode(self.game.ui)
