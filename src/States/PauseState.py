c59ce1aa9fce61ea991076f30f699642ecb66012
matias <mat20can@gmail.com>
2026-02-09 16:59:18 -0300

    version 10

--- src/States/PauseState.py
+++ src/States/PauseState.py
@@ -19,3 +19,6 @@ class PauseState(State):
         if ui_main == GameState.PLAYING: self.game.state_manager.change(PlayingState(self.game))
         elif ui_main == GameState.MENU: self.game.state_manager.change(MenuState(self.game))
         elif ui_main == GameState.OPTIONS: self.game.state_manager.change(OptionsState(self.game))
+    def draw(self, surface):
+        self.game.visuals_items.draw()
+        self.menu.render()
