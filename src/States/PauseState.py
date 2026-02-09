df90249e40646a21e3a2a1f8d407f4d22c601ded
matias <mat20can@gmail.com>
2026-02-09 16:58:33 -0300

    version 10

--- src/States/PauseState.py
+++ src/States/PauseState.py
@@ -8,3 +8,8 @@ class PauseState(State):
     def __init__(self, game):
         super().__init__(game)
         self.menu = Pause(self.game.ui)
+    def enter(self, params=None):
+        self.game.main = GameState.PAUSE
+        self.game.ui.main = GameState.PAUSE
+        self.game.ui.setup_button_factories()
+        self.menu.setup_buttons()
