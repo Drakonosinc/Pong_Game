4ff801c7b6525da54508a24eae4ca7b30627ffc7
matias <mat20can@gmail.com>
2026-02-11 19:47:07 -0300

    version 10

--- src/States/StateManager.py
+++ src/States/StateManager.py
@@ -1,18 +1,29 @@
 class StateManager:
-    def __init__(self):
+    def __init__(self, state_factory):
         self.stack = []
-    def push(self, state, params=None):
-        self.stack.append(state)
-        state.enter(params)
-    def pop(self):
+        self.factory = state_factory
+    def change_state(self, state_enum, params=None):
+        new_state = self.factory.get_state(state_enum)
+        if new_state is None:
+            print(f"Error: StateFactory devolvió None para {state_enum}")
+            return
+        if self.stack:
+            previous_state = self.stack.pop()
+            previous_state.exit()
+        self.stack.append(new_state)
+        new_state.enter(params)
+    def push_state(self, state_enum, params=None):
+        new_state = self.factory.get_state(state_enum)
+        if new_state:
+            self.stack.append(new_state)
+            new_state.enter(params)
+    def pop_state(self):
         if self.stack:
             state = self.stack.pop()
             state.exit()
             return state
         return None
-    def change(self, state, params=None):
-        if self.stack: self.pop()
-        self.push(state, params)
+
     def update(self, dt):
         if self.stack: self.stack[-1].update(dt)
     def draw(self, surface):
