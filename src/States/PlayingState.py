fcb3a093913873e022829231b136a240c8b992bf
matias <mat20can@gmail.com>
2026-02-10 22:41:09 -0300

    version 10

--- src/States/PlayingState.py
+++ src/States/PlayingState.py
@@ -1,3 +1,6 @@
 from .State import State
 from Utils.States import GameState
 from .GameOverState import GameOverState
+class PlayingState(State):
+    def __init__(self, game):
+        super().__init__(game)
