e5db7ed9116578fa32ad0d9a28a969ec4b05aafc
matias <mat20can@gmail.com>
2026-02-12 15:41:04 -0300

    version 10

--- src/States/PauseState.py
+++ src/States/PauseState.py
@@ -5,3 +5,6 @@ class PauseState(State):
     def __init__(self, game):
         super().__init__(game)
         self.menu = Pause(self.game.ui)
+    def enter(self, params=None):
+        self.game.main = GameState.PAUSE
+        self.game.ui.main = GameState.PAUSE
