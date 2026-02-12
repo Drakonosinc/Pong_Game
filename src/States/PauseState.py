f5d2ae140685a489d3a45ad66bc16e7db3ae486e
matias <mat20can@gmail.com>
2026-02-12 15:40:56 -0300

    version 10

--- src/States/PauseState.py
+++ src/States/PauseState.py
@@ -1,3 +1,7 @@
 from .State import State
 from Utils.States import GameState
 from Interface.Menus.Pause import Pause
+class PauseState(State):
+    def __init__(self, game):
+        super().__init__(game)
+        self.menu = Pause(self.game.ui)
