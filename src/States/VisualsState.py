8dce237c7a81f4c71b2651055171784d523628a2
matias <mat20can@gmail.com>
2026-02-12 15:45:12 -0300

    version 10

---
+++ src/States/VisualsState.py
@@ -0,0 +1,12 @@
+from .State import State
+from Utils.States import GameState
+from Interface.Menus.Visuals_Menu import VisualsMenu
+class VisualsState(State):
+    def __init__(self, game):
+        super().__init__(game)
+        self.menu = VisualsMenu(self.game.ui)
+    def enter(self, params=None):
+        self.game.main = GameState.VISUALS
+        self.game.ui.main = GameState.VISUALS
+        self.game.ui.setup_button_factories()
+        self.menu.setup_buttons()
