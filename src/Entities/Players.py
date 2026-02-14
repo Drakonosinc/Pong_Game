634f64e25c424b6517174532fdb054e9da99d840
matias <mat20can@gmail.com>
2026-02-14 16:06:36 -0300

    version 10

--- src/Entities/Players.py
+++ src/Entities/Players.py
@@ -1,13 +1,13 @@
-from pygame import *
+from src.Core.Domain.Geometry import Rectangle
 class Player:
     def __init__(self, x, y, width, height, active):
-        self.rect = Rect(x, y, width, height)
+        self.rect = Rectangle(x, y, width, height)
         self.active = active
         self.score = 0
         self.reward = 0
         self.reset_position = (x, y, width, height)
     def reset(self):
-        self.rect = Rect(*self.reset_position)
+        self.rect = Rectangle(*self.reset_position)
         self.active = [True] * len(self.active)
         self.score = 0
     def check_collision(self, other_rect): return self.rect.colliderect(other_rect)
