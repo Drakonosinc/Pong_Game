dc30b9c3f7c942a376882b6bd8a46fb9abe06413
matias <mat20can@gmail.com>
2026-02-14 16:02:54 -0300

    version 10

--- src/Core/Interfaces/ISoundService.py
+++ src/Core/Interfaces/ISoundService.py
@@ -2,3 +2,5 @@ from abc import ABC, abstractmethod
 class IAudioService(ABC):
     @abstractmethod
     def play(self, sound_id: str, loop: bool = False) -> None: pass
+    @abstractmethod
+    def stop(self, sound_id: str) -> None: pass
