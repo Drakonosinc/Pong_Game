492e5735d1b57da89cab6bf3b036cc8b654c40d5
matias <mat20can@gmail.com>
2026-02-15 15:17:38 -0300

    version 10

--- src/Infrastructure/Services/PygameAudioService.py
+++ src/Infrastructure/Services/PygameAudioService.py
@@ -2,3 +2,4 @@ import pygame
 from src.Core.Interfaces.ISoundService import IAudioService
 class PygameAudioService(IAudioService):
     def __init__(self, asset_manager):
+        self.assets = asset_manager
