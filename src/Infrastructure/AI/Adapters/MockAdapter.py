2069f6332fd4702643ac56bbaca0925cbf42f0d6
matias <mat20can@gmail.com>
2026-02-17 14:11:48 -0300

    version 10

--- src/Infrastructure/AI/Adapters/MockAdapter.py
+++ src/Infrastructure/AI/Adapters/MockAdapter.py
@@ -3,3 +3,4 @@ from src.Core.Interfaces.IAIModel import IAIModel
 class MockAdapter(IAIModel):
     def __init__(self, output_size: int = 2): self.output_size = output_size
     def predict(self, state: np.ndarray) -> np.ndarray: return np.random.uniform(-1, 1, self.output_size)
+    def save(self, path: str): print(f"[MockAdapter] 'Saving' model to {path}")
