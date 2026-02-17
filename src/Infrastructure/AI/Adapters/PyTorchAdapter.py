82be20e515fba7d9daecf8ab42c315ef61363dd6
matias <mat20can@gmail.com>
2026-02-17 14:07:03 -0300

    version 10

--- src/Infrastructure/AI/Adapters/PyTorchAdapter.py
+++ src/Infrastructure/AI/Adapters/PyTorchAdapter.py
@@ -4,3 +4,4 @@ from src.Core.Interfaces.IAIModel import IAIModel
 class PyTorchAdapter(IAIModel):
     def __init__(self, model: torch.nn.Module):
         self.model = model
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
