8921189a85f956b33d4f1170b27d4a59b5167609
matias <mat20can@gmail.com>
2026-02-17 14:11:36 -0300

    version 10

--- src/Infrastructure/AI/Adapters/MockAdapter.py
+++ src/Infrastructure/AI/Adapters/MockAdapter.py
@@ -2,3 +2,4 @@ import numpy as np
 from src.Core.Interfaces.IAIModel import IAIModel
 class MockAdapter(IAIModel):
     def __init__(self, output_size: int = 2): self.output_size = output_size
+    def predict(self, state: np.ndarray) -> np.ndarray: return np.random.uniform(-1, 1, self.output_size)
