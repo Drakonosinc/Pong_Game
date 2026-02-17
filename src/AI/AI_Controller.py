e7aefcacdb17ba1dd067e9a5eab0df8e1e901521
matias <mat20can@gmail.com>
2026-02-17 14:15:32 -0300

    version 10

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -1,2 +1,9 @@
 import numpy as np
 from src.Core.Interfaces.IAIModel import IAIModel
+class AIHandler:
+    def __init__(self, game):
+        self.game = game
+        self.prev_state = None
+        self.prev_action = None
+        self.prev_reward = 0
+        self.model_adapter: IAIModel = None 
