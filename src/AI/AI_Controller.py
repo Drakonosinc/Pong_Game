5d4db00d0424a76fa79193361e4caaf1dc646fd7
matias <mat20can@gmail.com>
2026-02-19 18:18:43 -0300

    version 10

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -36,3 +36,4 @@ class AIHandler:
     def AI_actions(self, action):
         p2 = self.game.game_logic.player_two
         if action[0] > 0 and p2.rect.top > 0: p2.rect.y -= 5
+        if action[0] < 0 and p2.rect.bottom < self.game.HEIGHT: p2.rect.y += 5
