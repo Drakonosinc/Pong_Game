95df56b394f3dd9f1eac7a9b3ec72043c436f432
matias <mat20can@gmail.com>
2026-02-20 16:39:16 -0300

    version 10

--- src/Infrastructure/Training/TrainingAdapters.py
+++ src/Infrastructure/Training/TrainingAdapters.py
@@ -1,3 +1,4 @@
 import torch
 from src.Core.Interfaces.ITrainer import ITrainer
 from src.Type_Training.Genetic_Algorithm import genetic_algorithm, save_genetic_model
+from src.Type_Training.Q_learning import q_learning_algorithm, save_qlearning_model
