5c3ed9a4c3ee984bf8bcf131813cb33a8fb57f37
matias <mat20can@gmail.com>
2026-02-21 16:50:28 -0300

    version 10

--- src/Infrastructure/Training/TrainingAdapters.py
+++ src/Infrastructure/Training/TrainingAdapters.py
@@ -3,3 +3,4 @@ from src.Core.Interfaces.ITrainer import ITrainer
 from src.Type_Training.Genetic_Algorithm import genetic_algorithm, save_genetic_model
 from src.Type_Training.Q_learning import q_learning_algorithm, save_qlearning_model
 class GeneticTrainer(ITrainer):
+    def train(self, game):
