4bf7f66aefdc26807fdcf6a2720cb4a2b41f1bfa
matias <mat20can@gmail.com>
2026-02-24 13:19:53 -0300

    version 10

--- src/Infrastructure/Container.py
+++ src/Infrastructure/Container.py
@@ -4,3 +4,4 @@ from src.Infrastructure.AI.Adapters.MockAdapter import MockAdapter
 from src.Infrastructure.Training.TrainingAdapters import GeneticTrainer, QLearningTrainer
 from src.Game.Space_Pong import Space_pong_game
 class Container:
+    def __init__(self):
