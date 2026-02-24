80f53b6df987d8fb9174f99ec87c714dc1882cae
matias <mat20can@gmail.com>
2026-02-24 13:19:20 -0300

    version 10

--- src/Infrastructure/Container.py
+++ src/Infrastructure/Container.py
@@ -1,3 +1,4 @@
 import sys
 from src.Infrastructure.AI.Adapters.PyTorchAdapter import PyTorchAdapter
 from src.Infrastructure.AI.Adapters.MockAdapter import MockAdapter
+from src.Infrastructure.Training.TrainingAdapters import GeneticTrainer, QLearningTrainer
