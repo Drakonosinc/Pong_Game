9ffd3a0071c5fd73b1e32b59fc9000b4533fb36e
matias <mat20can@gmail.com>
2026-02-24 13:19:35 -0300

    version 10

--- src/Infrastructure/Container.py
+++ src/Infrastructure/Container.py
@@ -2,3 +2,4 @@ import sys
 from src.Infrastructure.AI.Adapters.PyTorchAdapter import PyTorchAdapter
 from src.Infrastructure.AI.Adapters.MockAdapter import MockAdapter
 from src.Infrastructure.Training.TrainingAdapters import GeneticTrainer, QLearningTrainer
+from src.Game.Space_Pong import Space_pong_game
