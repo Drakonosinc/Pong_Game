1654009a027d1ef4794314076af557df61d7fc79
matias <mat20can@gmail.com>
2026-03-02 13:56:16 -0300

    version 10

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pygame
 current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
 
 from src.Infrastructure.Container import Container
 def main():
