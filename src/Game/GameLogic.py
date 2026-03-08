46b8b2ce3e9d3b8368988169391d7c75b7a88b0c
matias <mat20can@gmail.com>
2026-03-08 17:18:54 -0300

    version 10

--- src/Game/GameLogic.py
+++ src/Game/GameLogic.py
@@ -43,3 +43,4 @@ class GameLogic:
             p2_x=self.player_two.rect.x, p2_y=self.player_two.rect.y,
             ball_x=self.balls[0].rect.x, ball_y=self.balls[0].rect.y)
         self.event_manager.post(GameStateChangedEvent(
+            state_dto=dto,
