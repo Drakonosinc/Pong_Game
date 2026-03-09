c7caa2263d51eb2894ac2d1ad4209809bf9131c9
matias <mat20can@gmail.com>
2026-03-09 15:25:11 -0300

    version 10

--- src/Game/GameLogic.py
+++ src/Game/GameLogic.py
@@ -53,3 +53,4 @@ class GameLogic:
         if isinstance(action, (int, np.integer)):
             if action == 0 and p2.rect.top > 0: p2.rect.y -= 5
             elif action == 1 and p2.rect.bottom < self.height: p2.rect.y += 5
+        else:
