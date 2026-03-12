a6eac677715c2c5f759bb7fa6b491ac43f9d4e01
matias <mat20can@gmail.com>
2026-03-12 14:47:26 -0300

    version 10

--- src/Game/GameLogic.py
+++ src/Game/GameLogic.py
@@ -77,4 +77,4 @@ class GameLogic:
         p1 = self.player_one
         if p1.rect.top > 0 or p1.rect.bottom < self.height: p1.rect.y += self.balls[0].move_y
         if p1.rect.y >= 310: p1.rect.y = 310
-        if p1.rect.y <= 0: 
+        if p1.rect.y <= 0: p1.rect.y = 0
