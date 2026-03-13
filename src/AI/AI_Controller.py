d92f13f0e94433e0e9ca74de26319c7ca496e67f
matias <mat20can@gmail.com>
2026-03-13 14:29:29 -0300

    version 10

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -3,3 +3,4 @@ from src.Core.Interfaces.IAIModel import IAIModel
 from src.Events.GameEvents import GameStateChangedEvent, ActionDecidedEvent
 class AIHandler:
     def __init__(self, game):
+        self.game = game
