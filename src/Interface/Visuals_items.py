d10e6277905381297bc321d9d71995a6a7ab1191
matias <mat20can@gmail.com>
2026-03-16 13:59:01 -0300

    version 10

--- src/Interface/Visuals_items.py
+++ src/Interface/Visuals_items.py
@@ -8,3 +8,4 @@ class Visuals_items:
         self.current_state_dto = None
         self.p1_score = 0
         self.p2_score = 0
+        self.game.event_manager.subscribe(GameStateChangedEvent, self.handle_state_changed)
