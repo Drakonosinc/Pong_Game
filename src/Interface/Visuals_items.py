e1a2a92e94e6c62fbad14fcb7359d0138182eb5b
matias <mat20can@gmail.com>
2026-03-17 13:36:22 -0300

    version 10

--- src/Interface/Visuals_items.py
+++ src/Interface/Visuals_items.py
@@ -19,3 +19,4 @@ class Visuals_items:
         screen.blit(self.assets.spacecraft2, (578, self.current_state_dto.p2_y - 140))
         self.rotated_ball = pygame.transform.rotate(self.assets.planet, self.current_state_dto.ball_x)
         screen.blit(self.rotated_ball, (self.current_state_dto.ball_x, self.current_state_dto.ball_y))
+    def mode_speed(self, screen):
