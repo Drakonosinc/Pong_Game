cd1c4abca37decf89bde28ad8ec594324e5240f2
matias <mat20can@gmail.com>
2026-03-20 13:39:08 -0300

    version 10

--- src/Interface/Visuals_items.py
+++ src/Interface/Visuals_items.py
@@ -54,3 +54,4 @@ class Visuals_items:
                 weights_preview = None
                 if hasattr(self.game.model, 'fc1'):
                     l = self.game.model.fc1
+                    if hasattr(l, 'weight'): weights_preview = l.weight.detach().cpu().numpy().flatten()[:5]
