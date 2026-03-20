10a8e4a312691a3f20db03df0098089cd4694ea3
matias <mat20can@gmail.com>
2026-03-20 13:38:34 -0300

    version 10

--- src/Interface/Visuals_items.py
+++ src/Interface/Visuals_items.py
@@ -50,3 +50,4 @@ class Visuals_items:
     def draw_model_data(self, screen):
         if self.game.mode_game["AI"]: self.game.model = self.game.model_training
         if self.game.model is not None:
+            try:
