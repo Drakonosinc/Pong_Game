dbe999a04aa02816bc8cc4ece85f4d1b1fda27ab
matias <mat20can@gmail.com>
2026-03-22 13:24:44 -0300

    version 10

--- src/Game/Space_Pong.py
+++ src/Game/Space_Pong.py
@@ -118,6 +118,7 @@ class Space_pong_game:
     def type_game(self):
         if self.mode_game["Training AI"]: self.game_logic.auto_play_player1()
     def item_repeat_run(self):
+        if not self.mode_game["Training AI"]:
         
     def run(self):
         self.running = True
