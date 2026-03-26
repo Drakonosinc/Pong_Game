d31a208fafb698dddc3d0c349cc067ea80d6732b
matias <mat20can@gmail.com>
2026-03-26 14:16:35 -0300

    version 10

--- src/Train_Headless.py
+++ src/Train_Headless.py
@@ -6,3 +6,4 @@ for path in (PROJECT_ROOT, SRC_ROOT):
     path_str = str(path)
     if path_str not in sys.path: sys.path.insert(0, path_str)
 from src.Loaders.Config_Loader import Config
+from src.Events.EventManager import EventManager
