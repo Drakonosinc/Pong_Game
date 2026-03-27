f8f60b7d5986a3dacad71df3f4faa060e1e75a29
matias <mat20can@gmail.com>
2026-03-27 16:32:05 -0300

    version 10

--- src/Train_Headless.py
+++ src/Train_Headless.py
@@ -23,3 +23,4 @@ class HeadlessEnvironment:
         self.HEIGHT = self.config.config_visuals["HEIGHT"]
         self.mode_game = {"Training AI": True, "Player": False, "AI": False}
         self.event_manager = EventManager()
+        self.audio_service = MockAudioService()
