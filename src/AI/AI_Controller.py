84ec8aa8ecd36c6fd2948d5ee8aa34a0b10c1dbe
matias <mat20can@gmail.com>
2026-04-02 14:56:29 -0300

    version 10

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -15,6 +15,7 @@ class AIHandler:
         if model is None: return
         if isinstance(model, IAIModel):
             self.model_adapter = model
+            return
 
     def handle_game_state_changed(self, event: GameStateChangedEvent):
         state_array = event.state_dto.to_array()
