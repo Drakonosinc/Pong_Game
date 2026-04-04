0e25f1d9ae1fe3b784bf1777a377a2a830b30590
matias <mat20can@gmail.com>
2026-04-04 15:13:45 -0300

    version 10

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -3,3 +3,7 @@ from .Elements_interface import *
 from Interface.Menus.Base_Menu import BaseMenu 
 from Events.GameEvents import QuitEvent
 from Loaders.AILoader import AILoader
+class Interface(BaseMenu):
+    def __init__(self, context):
+        self.context = context
+        self.game = None
