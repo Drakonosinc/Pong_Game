7b37e16b3f5c2a6aa52b95b21d04833a4457b797
matias <mat20can@gmail.com>
2026-04-06 14:53:26 -0300

    version 10

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -37,3 +37,4 @@ class Interface(BaseMenu):
         self.planet = assets.planet
         self.spacecraft = assets.spacecraft
         self.spacecraft2 = assets.spacecraft2
+        self.GRAY = assets.GRAY
