06a2d6dc375863a20c77f153f992dc610a4e1ef5
matias <mat20can@gmail.com>
2026-04-08 15:15:17 -0300

    version 10

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -64,3 +64,4 @@ class Interface(BaseMenu):
             "sound_hover": self.sound_buttonletters,
             "sound_touch": self.sound_touchletters})
         self.button_factory_f2_5 = ElementsFactory({
+            "screen": self.screen,
