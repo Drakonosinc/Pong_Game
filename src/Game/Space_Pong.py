d2c36c2e32a139ecedfa75b313e96587b4665775
matias <mat20can@gmail.com>
2026-04-13 15:18:44 -0300

    version 10

--- src/Game/Space_Pong.py
+++ src/Game/Space_Pong.py
@@ -97,6 +97,7 @@ class Space_pong_game:
     def shutdown(self):
         if self._shutdown_done: return
         self._shutdown_done = True
+        self.running = False
 
     def change_speed(self, fps, speed, number, objet, speed_down=True, speed_up=True):
         self.FPS += fps
