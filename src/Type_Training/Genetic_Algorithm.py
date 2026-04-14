8d1d8757453df845d9a8072856b140c542dcbc1e
matias <mat20can@gmail.com>
2026-04-14 15:19:54 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -55,6 +55,7 @@ def evaluate_population(population, game, num_trials=3):
         for _ in range(num_trials):
             score.append(fitness_function(model, game))
             if game.exit: break
+        if not score: break
 
 def select_parents(population, fitness_scores, num_parents):
     sorted_pop_fitness = sorted(zip(population, fitness_scores), key=lambda x: x[1], reverse=True)
