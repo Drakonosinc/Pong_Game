8bb59582ff41bb76a7442219f0d2b74d8c79999c
matias <mat20can@gmail.com>
2026-04-14 15:18:44 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -46,6 +46,7 @@ def initialize_population(type_model, size, input_size, output_size, hidden_size
         population.append(model)
     return population
 
+def evaluate_population(population, game, num_trials=3):
 
 def select_parents(population, fitness_scores, num_parents):
     sorted_pop_fitness = sorted(zip(population, fitness_scores), key=lambda x: x[1], reverse=True)
