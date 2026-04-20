01954ddc8f4cab322eb4cae04859037a44ee739f
matias <mat20can@gmail.com>
2026-04-20 17:16:31 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -43,3 +43,4 @@ def _set_weights_np(model, weights):
     raise TypeError("Unsupported model type for genetic algorithm")
 def _ensure_built_if_tf(model, input_size):
     if _is_tf_model(model):
+        try:
