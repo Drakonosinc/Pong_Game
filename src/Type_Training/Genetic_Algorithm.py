e3d41149b161ea960984abc9839754846b306c58
matias <mat20can@gmail.com>
2026-04-21 18:29:18 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -64,3 +64,4 @@ def initialize_population(type_model, size, input_size, output_size, hidden_size
 def evaluate_population(population, game, num_trials=3):
     evaluated_population = []
     fitness_scores = []
+    for model in population:
