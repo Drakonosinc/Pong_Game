ed101c8396c00835da20019b56b97ffd9876cb83
matias <mat20can@gmail.com>
2026-04-22 14:53:56 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -85,3 +85,4 @@ def select_parents(population, fitness_scores, num_parents):
     top_fitness = [fitness for _, fitness in sorted_pop_fitness[:top_count]]
     return random.choices(top_population, weights=top_fitness, k=num_parents)
 def _new_model(type_model, input_size, output_size, hidden_sizes):
+    model = _build_model(type_model, input_size, output_size, hidden_sizes=hidden_sizes)
