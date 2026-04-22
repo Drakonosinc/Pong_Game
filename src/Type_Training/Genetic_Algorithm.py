5d6884a4ea971e45c563fd77947add77426041ba
matias <mat20can@gmail.com>
2026-04-22 14:53:02 -0300

    version 10

--- src/Type_Training/Genetic_Algorithm.py
+++ src/Type_Training/Genetic_Algorithm.py
@@ -80,3 +80,4 @@ def evaluate_population(population, game, num_trials=3):
     return evaluated_population, fitness_scores
 def select_parents(population, fitness_scores, num_parents):
     sorted_pop_fitness = sorted(zip(population, fitness_scores), key=lambda x: x[1], reverse=True)
+    top_count = max(2, len(sorted_pop_fitness) // 2)
