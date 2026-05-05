bc87db5244686a6d04be335aa3a62ce983bb46e7
matias <mat20can@gmail.com>
2026-05-05 15:50:12 -0300

    version 10

--- src/Loaders/AILoader.py
+++ src/Loaders/AILoader.py
@@ -18,3 +18,4 @@ class AILoader:
         return training
     def _get_selected_model(self) -> str:
         model = next((key for key, value in self.config.config_AI["type_model"].items() if value), None)
+        if model is None: raise ValueError("No se especifico un tipo de modelo valido.")
