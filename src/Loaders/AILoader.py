5dcbf6da408e25263182f2095336a17891ff88be
matias <mat20can@gmail.com>
2026-05-20 15:28:04 -0300

    version 10

--- src/Loaders/AILoader.py
+++ src/Loaders/AILoader.py
@@ -51,5 +51,4 @@ class AILoader:
             return AIModelLoadResult(model=model, model_found=True, error_message=None)
         except Exception as exc:
             return AIModelLoadResult(model=None, model_found=False, error_message=str(exc))
-    def load_model(self):
-        return self.load_model_result().model
+    def load_model(self): return self.load_model_result().model
